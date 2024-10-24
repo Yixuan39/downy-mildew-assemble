abbf291418a490bde4116c806fdbf617a565f0b8
Yixuan Yang <yyang55@ncsu.edu>
2024-10-24 00:29:25 -0400

    Update busco.R

--- code/busco.R
+++ code/busco.R
@@ -1,12 +1,17 @@
-library(tidyverse)
+#!/usr/bin/env Rscript
 library(cogeqc)
-library(here)
 
 files <- list.files('~/project_data/downy/ref-seq/', full.names = TRUE)
-outpath <- here('busco_results')
-if (!dir.exists(outpath)) {dir.create(outpath, showWarnings = FALSE)}
+outpath <- '~/project_data/downy/busco_results'
+if (!dir.exists(outpath)) {dir.create(outpath)}
 max.core <- 24
 
 for (file in files){
-    run_busco(file, outlabel = basename(file), mode = 'genome', auto_lineage = 'prok', outpath = outpath, force = TRUE, threads = max.core, download_path = '~/project_data/downy/busco_downloads')
+    run_busco(file,
+              outlabel = basename(file),
+              mode = 'genome', auto_lineage = 'prok',
+              outpath = outpath, 
+              force = TRUE, 
+              threads = max.core,
+              download_path = '~/project_data/downy/busco_downloads')
 }
