a4d46efe1c29a46e306b3748734a1c5dece74129
Yixuan Yang <yyang55@ncsu.edu>
2024-10-31 22:03:15 -0400

    translate reference genome tool

---
+++ code/translate.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import os
+import argparse
+
+def transeq(input_file, output_file):
+    command = 'transeq -sequence ' + input_file + ' -outseq ' + output_file + ' -frame 6'
+    os.system(command)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Translate DNA to protein')
+    parser.add_argument('input_folder', help='Input file in FASTA format')
+    parser.add_argument('output_folder', help='Output file in FASTA format')
+    args = parser.parse_args()
+
+    # list fasta files in folder
+    files = os.listdir(args.input_folder)
+    os.makedirs(args.output_folder, exist_ok=True)
+    for file in files:
+        input_file = os.path.join(args.input_folder, file)
+        output_file = os.path.join(args.output_folder, file)
+        transeq(input_file, output_file)
