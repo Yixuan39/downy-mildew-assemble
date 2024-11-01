9a63fab45b6ed284af988001b60b1da87848b698
Yixuan Yang <yyang55@ncsu.edu>
2024-10-31 22:15:38 -0400

    Update diamond.sh

--- code/diamond.sh
+++ code/diamond.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --array=0-53
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 
 FILES=("MSU1" "Phumuli" "SC1982")
-refFiles=( $(ls ~/project_data/downy/ref-seq-prot) )
+# only use the files that do not have "dmnd" in the name
+refFiles=( $(ls ~/project_data/downy/ref-seq-prot | grep -v "dmnd") )
 
 INPUT_FILE=${FILES[$((SLURM_ARRAY_TASK_ID % ${#FILES[@]}))]}  # Cycle through FILES array
 REF_FILE=${refFiles[$((SLURM_ARRAY_TASK_ID % ${#refFiles[@]}))]}  # Cycle through refFiles array
@@ -14,5 +15,5 @@ python pacbio_cleaning.py \
   --input ~/project_data/downy/asm/${INPUT_FILE}.fasta \
   --ref ~/project_data/downy/ref-seq-prot/${REF_FILE} \
   --output ~/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
-  --threads 10 \
+  --threads 8 \
   --busco-downloads-path ~/project_data/downy/busco_downloads
