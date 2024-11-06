6225b5daa261c379ab8ea74636e15103c20f9137
Yixuan Yang <yyang55@ncsu.edu>
2024-11-06 11:47:33 -0500

    Update metaMDBG.sh

--- code/metaMDBG.sh
+++ code/metaMDBG.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#SBATCH --array=0-2
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
 
 # This script will run MetaMDBG on the downy mildew data
-# List of files to process (without the `.fastq.gz` extension)
-FILES=("MSU1" "Phumuli" "SC1982")  # Add your file names here
+FILES=("MSU1" "Phumuli" "SC1982")
 
-# Get the file name based on the task ID
-FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
-
-metaMDBG asm \
---out-dir ~/project_data/downy/meta-asm/${FILE} \
---in-hifi ~/project_data/downy/data/${FILE}.fastq.gz \
---threads 24
+for FILE in "${FILES[@]}"; do
+  metaMDBG asm \
+  --out-dir ~/project_data/downy/meta-asm/${FILE} \
+  --in-hifi ~/project_data/downy/data/${FILE}.fastq.gz \
+  --threads 24
+done
