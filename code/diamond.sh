311eb89e2e503fff7a2f2fe8516dd9019a1aac2a
Yixuan39 <71982982+Yixuan39@users.noreply.github.com>
2024-11-06 11:45:07 -0500

    directly use diamond blast

--- code/diamond.sh
+++ code/diamond.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=0-53
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=40G
 
 
 FILES=("MSU1" "Phumuli" "SC1982")
@@ -22,10 +22,22 @@ REF_FILE=${refFiles[$REF_INDEX]}
 # Ensure the output directory exists
 mkdir -p ~/project_data/downy/diamond/${REF_FILE}
 
-# Run the python script with the corresponding file name
-python pacbio_cleaning.py \
-  --input ~/project_data/downy/asm/${INPUT_FILE}.fasta \
-  --ref ~/project_data/downy/ref-seq-prot/${REF_FILE} \
-  --output ~/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
-  --threads 8 \
-  --busco_downloads_path ~/project_data/downy/busco_downloads
+diamond blastx \
+  --db ~/project_data/downy/ref-seq-prot/${REF_FILE} \
+  --query ~/project_data/downy/meta-asm/${INPUT_FILE}/contigs.fasta.gz \
+  --out ~/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.csv \
+  --al ~/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
+  --evalue 1e-10 \
+  --max-target-seqs 1 \
+  --max-hsps 1 \
+  --very-sensitive \
+  --outfmt 6 \
+  --threads 8
+
+## Run the python script with the corresponding file name
+#python pacbio_cleaning.py \
+#  --input ~/project_data/downy/asm/${INPUT_FILE}.fasta \
+#  --ref ~/project_data/downy/ref-seq-prot/${REF_FILE} \
+#  --output ~/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
+#  --threads 8 \
+#  --busco_downloads_path ~/project_data/downy/busco_downloads
