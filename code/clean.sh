dc4ff8f95e74b19628ed088c1c8ad064d20c4ae7
Yixuan Yang <yyang55@ncsu.edu>
2024-11-07 13:41:39 -0500

    Update clean.sh

--- code/clean.sh
+++ code/clean.sh
@@ -21,11 +21,11 @@ REF_FILE=${refFiles[$REF_INDEX]}
 # Ensure the output directory exists
 mkdir -p ~/project_data/downy/Kraken/${REF_FILE}
 
-for INPUT_FILE in "${FILES[@]}"; do
-  kraken2-build --download-taxonomy --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein --use-ftp
-  kraken2-build --add-to-library ~/project_data/downy/ref-seq-prot/${REF_FILE} --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein
-  kraken2-build --build --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein
+kraken2-build --download-taxonomy --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein --use-ftp --skip-maps
+kraken2-build --add-to-library ~/project_data/downy/ref-seq-prot/${REF_FILE} --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein
+kraken2-build --build --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb --protein
 
+for INPUT_FILE in "${FILES[@]}"; do
   kraken2 --db ~/project_data/downy/ref-seq-prot/${REF_FILE}_kdb \
     --threads 24 \
     --output - \
