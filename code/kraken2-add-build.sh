865414a1db4ef9fa8e0740974042958e27f0648f
Yixuan Yang <yyang55@ncsu.edu>
2024-11-14 12:32:37 -0500

    Update kraken2-add-build.sh

--- code/kraken2-add-build.sh
+++ code/kraken2-add-build.sh
@@ -9,13 +9,13 @@ for REF_FILE in "${refFiles[@]}"; do
     echo "adding ${REF_FILE}..."
     kraken2-build --add-to-library ~/project_data/downy/ref-seq-prot/${REF_FILE} --db ~/project_data/downy/KrakenDB-prot --protein
 done
-
-contamFiles=( $(ls ~/project_data/downy/contam-prot) )
+# add possible contaminant protein sequences to the Kraken2 database
+refFiles=( $(ls ~/project_data/downy/contam-prot) )
 for REF_FILE in "${refFiles[@]}"; do
     echo "adding ${REF_FILE}..."
     kraken2-build --add-to-library ~/project_data/downy/contam-prot/${REF_FILE} --db ~/project_data/downy/KrakenDB-prot --protein
 done
 
-kraken2-build --build --db KrakenDB-prot --protein --threads 24
-bracken-build -l 50 -d KrakenDB-prot -t 24
-bracken-build -l 200 -d KrakenDB-prot -t 24
+kraken2-build --build --db ~/project_data/downy/KrakenDB-prot --protein --threads 24
+bracken-build -l 50 -d ~/project_data/downy/KrakenDB-prot -t 24
+bracken-build -l 200 -d ~/project_data/downy/KrakenDB-prot -t 24
