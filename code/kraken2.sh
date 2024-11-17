7f6487239e1d0396871e9a3fc978470b1b4a7f1e
Yixuan Yang <yyang55@ncsu.edu>
2024-11-17 15:22:32 -0500

    don't use job array

--- code/kraken2.sh
+++ code/kraken2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#SBATCH --array=0-2
-#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=180G
 
@@ -8,37 +6,36 @@
 # List of files to process (without the `.fastq.gz` extension)
 FILES=("MSU1" "Phumuli" "SC1982")  # Add your file names here
 
-# Get the file name based on the task ID
-FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
+for FILE in "${FILES[@]}"; do
+  mkdir ~/project_data/downy/Kraken-raw
+  # Run kraken2 on raw reads
+  kraken2 --db ~/project_data/downy/KrakenDB-prot \
+    --threads 24 \
+    --output ~/project_data/downy/Kraken-raw/${FILE}.kraken \
+    --report ~/project_data/downy/Kraken-raw/${FILE}.kreport \
+    --gzip-compressed \
+    ~/project_data/downy/data/${FILE}.fastq.gz
 
-mkdir ~/project_data/downy/Kraken-raw
-# Run kraken2 on raw reads
-kraken2 --db ~/project_data/downy/KrakenDB-prot \
-  --threads 24 \
-  --output ~/project_data/downy/Kraken-raw/${FILE}.kraken \
-  --report ~/project_data/downy/Kraken-raw/${FILE}.kreport \
-  --gzip-compressed \
-  ~/project_data/downy/data/${FILE}.fastq.gz
+  bracken -d ~/project_data/downy/KrakenDB-prot \
+    -i ~/project_data/downy/Kraken-raw/${FILE}.kreport \
+    -o ~/project_data/downy/Kraken-raw/${FILE}.bracken \
+    -w ~/project_data/downy/Kraken-raw/${FILE}.breport \
+    -r 50 \
+    -l S
 
-bracken -d ~/project_data/downy/KrakenDB-prot \
-  -i ~/project_data/downy/Kraken-raw/${FILE}.kreport \
-  -o ~/project_data/downy/Kraken-raw/${FILE}.bracken \
-  -w ~/project_data/downy/Kraken-raw/${FILE}.breport \
-  -r 50 \
-  -l S
+  mkdir ~/project_data/downy/Kraken-asm
+  # Run bracken on the assembly data
+  kraken2 --db ~/project_data/downy/KrakenDB-prot \
+    --threads 24 \
+    --output ~/project_data/downy/Kraken-asm/${FILE}.kraken \
+    --report ~/project_data/downy/Kraken-asm/${FILE}.kreport \
+    --gzip-compressed \
+    ~/project_data/downy/meta-asm/${FILE}/contigs.fasta.gz
 
-mkdir ~/project_data/downy/Kraken-asm
-# Run bracken on the assembly data
-kraken2 --db ~/project_data/downy/KrakenDB-prot \
-  --threads 24 \
-  --output ~/project_data/downy/Kraken-asm/${FILE}.kraken \
-  --report ~/project_data/downy/Kraken-asm/${FILE}.kreport \
-  --gzip-compressed \
-  ~/project_data/downy/meta-asm/${FILE}/contigs.fasta.gz
-
-bracken -d ~/project_data/downy/KrakenDB-prot \
-  -i ~/project_data/downy/Kraken-asm/${FILE}.kreport \
-  -o ~/project_data/downy/Kraken-asm/${FILE}.bracken \
-  -w ~/project_data/downy/Kraken-asm/${FILE}.breport \
-  -r 200 \
-  -l S
+  bracken -d ~/project_data/downy/KrakenDB-prot \
+    -i ~/project_data/downy/Kraken-asm/${FILE}.kreport \
+    -o ~/project_data/downy/Kraken-asm/${FILE}.bracken \
+    -w ~/project_data/downy/Kraken-asm/${FILE}.breport \
+    -r 200 \
+    -l S
+done
