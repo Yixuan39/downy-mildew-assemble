899180c64046fd8c001a67bfc75101c7ea293ede
Yixuan Yang <yyang55@ncsu.edu>
2024-11-18 22:08:20 -0500

    add work path

--- code/kraken2.sh
+++ code/kraken2.sh
@@ -5,37 +5,38 @@
 # this script will run kraken2 on the downy mildew data against the large protein database
 # List of files to process (without the `.fastq.gz` extension)
 FILES=("MSU1" "Phumuli" "SC1982")  # Add your file names here
+WORK_PATH="/data/run/yyang"
 
 for FILE in "${FILES[@]}"; do
-  mkdir ~/project_data/downy/Kraken-raw
+  mkdir ${WORK_PATH}/project_data/downy/Kraken-raw
   # Run kraken2 on raw reads
-  kraken2 --db ~/project_data/downy/KrakenDB-prot \
+  kraken2 --db ${WORK_PATH}/project_data/downy/KrakenDB-prot \
     --threads 24 \
-    --output ~/project_data/downy/Kraken-raw/${FILE}.kraken \
-    --report ~/project_data/downy/Kraken-raw/${FILE}.kreport \
+    --output ${WORK_PATH}/project_data/downy/Kraken-raw/${FILE}.kraken \
+    --report ${WORK_PATH}/project_data/downy/Kraken-raw/${FILE}.kreport \
     --gzip-compressed \
-    ~/project_data/downy/data/${FILE}.fastq.gz
+    ${WORK_PATH}/project_data/downy/data/${FILE}.fastq.gz
 
-  bracken -d ~/project_data/downy/KrakenDB-prot \
-    -i ~/project_data/downy/Kraken-raw/${FILE}.kreport \
-    -o ~/project_data/downy/Kraken-raw/${FILE}.bracken \
-    -w ~/project_data/downy/Kraken-raw/${FILE}.breport \
+  bracken -d ${WORK_PATH}/project_data/downy/KrakenDB-prot \
+    -i ${WORK_PATH}/project_data/downy/Kraken-raw/${FILE}.kreport \
+    -o ${WORK_PATH}/project_data/downy/Kraken-raw/${FILE}.bracken \
+    -w ${WORK_PATH}/project_data/downy/Kraken-raw/${FILE}.breport \
     -r 50 \
     -l S
 
-  mkdir ~/project_data/downy/Kraken-asm
+  mkdir ${WORK_PATH}/project_data/downy/Kraken-asm
   # Run bracken on the assembly data
-  kraken2 --db ~/project_data/downy/KrakenDB-prot \
+  kraken2 --db ${WORK_PATH}/project_data/downy/KrakenDB-prot \
     --threads 24 \
-    --output ~/project_data/downy/Kraken-asm/${FILE}.kraken \
-    --report ~/project_data/downy/Kraken-asm/${FILE}.kreport \
+    --output ${WORK_PATH}/project_data/downy/Kraken-asm/${FILE}.kraken \
+    --report ${WORK_PATH}/project_data/downy/Kraken-asm/${FILE}.kreport \
     --gzip-compressed \
-    ~/project_data/downy/meta-asm/${FILE}/contigs.fasta.gz
+    ${WORK_PATH}/project_data/downy/meta-asm/${FILE}/contigs.fasta.gz
 
-  bracken -d ~/project_data/downy/KrakenDB-prot \
-    -i ~/project_data/downy/Kraken-asm/${FILE}.kreport \
-    -o ~/project_data/downy/Kraken-asm/${FILE}.bracken \
-    -w ~/project_data/downy/Kraken-asm/${FILE}.breport \
+  bracken -d ${WORK_PATH}/project_data/downy/KrakenDB-prot \
+    -i ${WORK_PATH}/project_data/downy/Kraken-asm/${FILE}.kreport \
+    -o ${WORK_PATH}/project_data/downy/Kraken-asm/${FILE}.bracken \
+    -w ${WORK_PATH}/project_data/downy/Kraken-asm/${FILE}.breport \
     -r 200 \
     -l S
 done
--- code/metaMDBG.sh
+++ code/metaMDBG.sh
@@ -2,10 +2,11 @@
 
 # This script will run MetaMDBG on the downy mildew data
 FILES=("MSU1" "Phumuli" "SC1982")
+WORK_PATH="/data/run/yyang"
 
 for FILE in "${FILES[@]}"; do
   metaMDBG asm \
-  --out-dir ~/project_data/downy/meta-asm/${FILE} \
-  --in-hifi ~/project_data/downy/data/${FILE}.fastq.gz \
+  --out-dir ${WORK_PATH}/project_data/downy/meta-asm/${FILE} \
+  --in-hifi ${WORK_PATH}/project_data/downy/data/${FILE}.fastq.gz \
   --threads 24
 done
