cf799133b5f2c8e81b724af7963ec4d861233a04
Yixuan Yang <yyang55@ncsu.edu>
2024-11-19 15:55:58 -0500

    Create diamond-build.R

---
+++ code/diamond-build.R
@@ -0,0 +1,19 @@
+library(Biostrings)
+library(stringr)
+
+ref.seq.files <- list.files(file.path('/data/run/yyang/project_data/downy/ref-seq-prot'), full.names = TRUE)
+db.path <- file.path('/data/run/yyang/project_data/downy/diamond/oomycete')
+genome.list <- AAStringSetList()
+fasta.file <- tempfile(fileext = '.fasta')
+
+
+for (file in ref.seq.files) {
+    # annotate names
+    seq <- readAAStringSet(file)
+    names(seq) <- str_c(fs::path_ext_remove(basename(file)),'$', names(seq))
+    genome.list[[file]] <- seq
+}
+
+writeXStringSet(unlist(unname(genome.list)), fasta.file)
+build.diamond <- paste0('diamond makedb --in ', fasta.file, ' --db ', db.path, ' --threads 10')
+message(build.diamond); system(build.diamond)
