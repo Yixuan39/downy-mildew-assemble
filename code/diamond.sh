3ced863fdc7360647f2c809dd55563c5a92dee04
Yixuan Yang <yyang55@ncsu.edu>
2024-11-19 16:54:34 -0500

    Update diamond.sh

--- code/diamond.sh
+++ code/diamond.sh
@@ -6,7 +6,7 @@
 FILES=("MSU1" "Phumuli" "SC1982")
 WORK_PATH="/data/run/yyang"
 # refFiles=( $(ls ${WORK_PATH}/project_data/downy/ref-seq-prot | grep -v "dmnd") )
-REF_FILE="/data/run/yyang/project_data/downy/diamond/oomycete.dmnd"
+REF_FILE="oomycete.dmnd"
 threads=32
 
 # Loop through each combination of FILES and refFiles
@@ -24,10 +24,10 @@ for INPUT_FILE in "${FILES[@]}"; do
 
     # Run DIAMOND blastx
     diamond blastx \
-        --db ${WORK_PATH}/project_data/downy/ref-seq-prot/${REF_FILE} \
+        --db ${WORK_PATH}/project_data/downy/diamond/${REF_FILE} \
         --query ${WORK_PATH}/project_data/downy/meta-asm/${INPUT_FILE}/contigs.fasta.gz \
-        --out ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.csv \
-        --al ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
+        --out ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}.csv \
+        --al ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}.fasta \
         --evalue 1e-10 \
         --max-target-seqs 1 \
         --max-hsps 1 \
@@ -37,8 +37,8 @@ for INPUT_FILE in "${FILES[@]}"; do
 
     # run busco
     # here we are not using auto-lineage, it will make the process fast and use less memory
-    busco -i ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta \
-        --out_path ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}_busco \
+    busco -i ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}.fasta \
+        --out_path ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}_busco \
         --mode genome \
         --auto-lineage-euk \
         --download_path ${WORK_PATH}/project_data/downy/busco_downloads \
@@ -47,10 +47,10 @@ for INPUT_FILE in "${FILES[@]}"; do
         --tar
 
     # run quast
-    quast.py --output-dir ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}_quast \
+    quast.py --output-dir ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}_quast \
         --threads ${threads} \
         --eukaryote \
-        ${WORK_PATH}/project_data/downy/diamond/${REF_FILE}/${INPUT_FILE}.fasta
+        ${WORK_PATH}/project_data/downy/diamond/${INPUT_FILE}.fasta
 
 
 done
