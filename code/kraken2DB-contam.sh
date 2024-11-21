25012bf77e7c3757c3fbceab8e6b5958deb46e0c
Yixuan Yang <yyang55@ncsu.edu>
2024-11-20 22:40:03 -0500

    add build db script

---
+++ code/kraken2DB-contam.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+WORK_PATH="/data/run/yyang"
+
+# Here we are not using the --protein flag, because we use protein sequences translated from the nucleotide sequences
+kraken2-build --download-taxonomy --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --use-ftp
+# download bacterial, fungi, human protein sequences to the Kraken2 database
+kraken2-build --download-library bacteria --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
+kraken2-build --download-library fungi --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
+kraken2-build --download-library human --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
+
+# add possible contaminant protein sequences to the Kraken2 database
+refFiles=( $(ls ${WORK_PATH}/project_data/downy/contam-prot) )
+for REF_FILE in "${refFiles[@]}"; do
+    echo "adding ${REF_FILE}..."
+    kraken2-build --add-to-library ${WORK_PATH}/project_data/downy/contam-prot/${REF_FILE} --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein
+done
+
+kraken2-build --build --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --threads 32
--- code/kraken2DB-whole.sh
+++ code/kraken2DB-whole.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=kraken2_build
-#SBATCH -c 32
-#SBATCH --mem=180G
 WORK_PATH="/data/run/yyang"
 
-kraken2-build --download-taxonomy --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
+# Here we are not using the --protein flag, because we use protein sequences translated from the nucleotide sequences
+kraken2-build --download-taxonomy --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --use-ftp
 # download bacterial, fungi, human protein sequences to the Kraken2 database
 kraken2-build --download-library bacteria --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
 kraken2-build --download-library fungi --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --use-ftp
@@ -24,5 +22,3 @@ for REF_FILE in "${refFiles[@]}"; do
 done
 
 kraken2-build --build --db ${WORK_PATH}/project_data/downy/KrakenDB-prot --protein --threads 32
-bracken-build -l 50 -d ${WORK_PATH}/project_data/downy/KrakenDB-prot -t 32
-bracken-build -l 200 -d ${WORK_PATH}/project_data/downy/KrakenDB-prot -t 32
