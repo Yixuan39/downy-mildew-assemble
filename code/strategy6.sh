69e094c500951d3cfbd25b149221a5a47c3f6540
Yixuan Yang <yyang55@ncsu.edu>
2024-11-23 11:01:54 -0500

    add diamond method

---
+++ code/strategy6.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+FILES=("MSU1" "Phumuli" "SC1982")  # Add your file names here
+WORK_PATH="/data/run/yyang"
+threads=32
+
+for FILE in "${FILES[@]}"; do
+    # Assemble the raw reads
+    metaMDBG asm \
+        --out-dir ${WORK_PATH}/project_data/downy/strategy6/${FILE}_asm \
+        --in-hifi  ${WORK_PATH}/project_data/downy/data/${FILE}.fastq.gz \
+        --threads ${threads}
+    # Run kraken2 on raw assembly
+    diamond blastx \
+        --db ${WORK_PATH}/project_data/downy/diamond/${REF_FILE} \
+        --query ${WORK_PATH}/project_data/downy/strategy6/${FILE}_asm/contigs.fasta \
+        --out ${WORK_PATH}/project_data/downy/strategy6/${INPUT_FILE}.csv \
+        --al ${WORK_PATH}/project_data/downy/strategy6/${FILE}.oomycota.fasta \
+        --alfmt fasta \
+        --header \
+        --long-reads \
+        --evalue 1e-10 \
+        --max-target-seqs 1 \
+        --max-hsps 1 \
+        --very-sensitive \
+        --outfmt 6 \
+        --threads ${threads}
+    # busco
+    busco -i ${WORK_PATH}/project_data/downy/strategy6/${FILE}.oomycota.fasta \
+        --out_path ${WORK_PATH}/project_data/downy/strategy6 \
+        --out ${FILE}_busco \
+        --mode genome \
+        --auto-lineage-euk \
+        --download_path ${WORK_PATH}/project_data/downy/busco_downloads \
+        --cpu ${threads} \
+        --force \
+        --tar
+    # quast
+    quast.py --output-dir ${WORK_PATH}/project_data/downy/strategy6/${FILE}_quast \
+        --threads ${threads} \
+        --eukaryote \
+        ${WORK_PATH}/project_data/downy/strategy6/${FILE}.oomycota.fasta
+done
