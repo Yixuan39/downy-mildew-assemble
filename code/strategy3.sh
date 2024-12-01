9256726e408b8516ab013f6bbaa6e83c45f77265
Yixuan Yang <yyang55@ncsu.edu>
2024-12-01 11:47:52 -0500

    Update strategy3.sh

--- code/strategy3.sh
+++ code/strategy3.sh
@@ -25,7 +25,7 @@ for FILE in "${FILES[@]}"; do
     # extract sequences classified as Oomycota (taxid 4762)
     extract_kraken_reads.py \
         -k ${RESULT_PATH}/${FILE}.kraken \
-        -s ${INPUT_FOLDER}/${FILE}.fastq.gz \
+        -s ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz \
         --taxid 4762 \
         --output ${RESULT_PATH}/${FILE}_asm.fasta \
         --report ${RESULT_PATH}/${FILE}.kreport \
