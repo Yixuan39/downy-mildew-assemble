ebb98fb423e16d5d9c5296000ecc73753d85e8fa
Yixuan Yang <yyang55@ncsu.edu>
2024-12-14 18:11:04 -0500

    Update strategy6-minimap2.sh

--- code/strategy6-minimap2.sh
+++ code/strategy6-minimap2.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
 
-FILES=("MSU1" "Phumuli" "SC1982")  # Add your file names here
-INPUT_FOLDER="/data/run/yyang/project_data/downy/data"
+FILES=("MSU1" "Phumuli" "SC1982")
+INPUT_FOLDER="/data/run/yyang/project_data/downy/metaMDBG"
 RESULT_PATH="/data/run/yyang/project_data/downy/strategy6"
-DB="/data/run/yyang/project_data/downy/diamond/oomycete.fasta"
-BUSCO_DB="/data/run/yyang/project_data/downy/BUSCO_DB"
+DB="/data/run/yyang/project_data/downy/combined_seqs/oomycota-genome.fasta.gz"
 threads=32
 mkdir -p ${RESULT_PATH}
 
 for FILE in "${FILES[@]}"; do
-    # Assemble the raw reads
-    metaMDBG asm \
-        --out-dir ${RESULT_PATH}/${FILE}_asm \
-        --in-hifi  ${INPUT_FOLDER}/${FILE}.fastq.gz \
-        --threads ${threads}
-    # Run minimap2 on raw assembly
-    minimap2 -ax map-hifi \
-        -t ${threads} \
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+        ${INPUT_FOLDER}/${FILE}.fasta \
         ${DB} \
-        ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz | \
-        samtools fasta -F 4 - > ${RESULT_PATH}/${FILE}_asm.fasta
-    # busco
-    busco -i ${RESULT_PATH}/${FILE}_asm.fasta \
-        --out_path ${RESULT_PATH} \
-        --out ${FILE}_busco \
-        --mode genome \
-        --auto-lineage-euk \
-        --download_path ${BUSCO_DB} \
-        --cpu ${threads} \
-        --force \
-        --tar
-    # quast
-    quast.py --output-dir ${RESULT_PATH}/${FILE}_quast \
-        --threads ${threads} \
-        --eukaryote \
-        ${RESULT_PATH}/${FILE}_asm.fasta
+        ${RESULT_PATH}/${FILE}.sam \
+        /tmp \
+        --search-type 3 --format-mode 1
+    # extract the reads
+    samtools fasta ${RESULT_PATH}/${FILE}.sam > ${RESULT_PATH}/${FILE}.fasta
 done
