843ef7547a995467ffcf3b67edfc8b35c26c82f3
Yixuan Yang <yyang55@ncsu.edu>
2024-12-14 21:30:55 -0500

    Create asm-mmseqs2-extract-genome.sh

---
+++ code/asm-mmseqs2-extract-genome.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INPUT_FOLDER="/Users/yixuanyang/project_data/downy/metaMDBG"
+RESULT_PATH="/Users/yixuanyang/project_data/downy/strategy6"
+DB="/Users/yixuanyang/project_data/downy/combined_seqs/oomycota-genome.fasta.gz"
+FILES=$(ls ${INPUT_FOLDER} | grep .fasta | sed 's/.fasta//g')
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+        ${INPUT_FOLDER}/${FILE}.fasta \
+        ${DB} \
+        ${RESULT_PATH}/${FILE}.sam \
+        ${RESULT_PATH}/${FILE}.tmp \
+        --search-type 3 --format-mode 1
+    # extract the reads
+    samtools fasta ${RESULT_PATH}/${FILE}.sam > ${RESULT_PATH}/${FILE}.fasta
+    rm ${RESULT_PATH}/${FILE}.sam
+    rm -rf ${RESULT_PATH}/${FILE}.tmp
+done
