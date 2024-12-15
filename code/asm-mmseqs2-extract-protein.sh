517f3e645e402c0b5731edb656c299eaca0f6de2
Yixuan Yang <yyang55@ncsu.edu>
2024-12-14 22:56:16 -0500

    use full read frame

--- code/asm-mmseqs2-extract-protein.sh
+++ code/asm-mmseqs2-extract-protein.sh
@@ -13,7 +13,7 @@ for FILE in ${FILES}; do
         ${DB} \
         ${RESULT_PATH}/${FILE}.sam \
         ${RESULT_PATH}/${FILE}.tmp \
-        --search-type 2 --format-mode 1
+        --search-type 2 --translation-mode 1 --format-mode 1
     # extract the reads
     samtools fasta ${RESULT_PATH}/${FILE}.sam > ${RESULT_PATH}/${FILE}.fasta
     rm ${RESULT_PATH}/${FILE}.sam
