dc0cb6b06ac10026367195291a0827a4d4b2f7f4
Yixuan Yang <yyang55@ncsu.edu>
2025-01-10 13:37:04 -0500

    Update quality_assessment.sh

--- code/quality_assessment.sh
+++ code/quality_assessment.sh
@@ -6,21 +6,19 @@ threads=32
 BUSCO_DB="/data/run/yyang/project_data/downy/BUSCO_DB"
 # get the fasta file full name
 files=$(find ${input_folder} -name "*.fasta")
-out_dirs=$(dirname ${files} | sort | uniq)
-echo ${out_dirs}
+dirs=$(dirname ${files} | sort | uniq)
 
-for out_dir in ${out_dirs}; do
-    mkdir -p ${out_dir}/busco
-    mkdir -p ${out_dir}/quast
-    busco --in ${out_dir} \
+for dir in ${dirs}; do
+    echo "Processing ${dir}"
+    busco --in ${dir} \
           --mode genome \
           --auto-lineage-euk \
           --download_path ${BUSCO_DB} \
           --cpu ${threads} \
           --force \
           --tar
-    quast.py --output-dir ${out_dir}/quast \
+    quast.py --output-dir ${dir}/quast \
             --threads ${threads} \
             --eukaryote \
-            ${out_dir}
+            ${dir}
 done
