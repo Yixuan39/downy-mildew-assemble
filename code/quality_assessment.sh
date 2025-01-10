15c6d54112da63b1404879b1c25ed696df7445ef
Yixuan Yang <yyang55@ncsu.edu>
2025-01-10 16:59:36 -0500

    Update quality_assessment.sh

--- code/quality_assessment.sh
+++ code/quality_assessment.sh
@@ -10,13 +10,21 @@ dirs=$(dirname ${files} | sort | uniq)
 
 for dir in ${dirs}; do
     echo "Processing ${dir}"
-    busco --in ${dir} \
-          --mode genome \
-          --auto-lineage-euk \
-          --download_path ${BUSCO_DB} \
-          --cpu ${threads} \
-          --force \
-          --tar
+#    busco --in ${dir} \
+#          --mode genome \
+#          --auto-lineage-euk \
+#          --download_path ${BUSCO_DB} \
+#          --cpu ${threads} \
+#          --force \
+#          --tar
+    files=$(find ${dir} -name "*.fasta")
+    for file in ${files}; do
+        compleasm --assembly_path ${file} \
+                  --output_dir $(dirname ${file})/compleasm \
+                  --library_path ${BUSCO_DB} \
+                  --threads ${threads} \
+                  --lineage eukaryota, stramenopiles_odb10
+    done
     quast.py --output-dir ${dir}/quast \
             --threads ${threads} \
             --eukaryote \
