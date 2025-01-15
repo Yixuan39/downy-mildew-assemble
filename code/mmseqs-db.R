697cf23d677eab2b9b527588038ba5a3937a6e7d
Yixuan Yang <yyang55@ncsu.edu>
2025-01-14 20:56:14 -0500

    Create mmseqs-db.R

---
+++ code/mmseqs-db.R
@@ -0,0 +1,22 @@
+library(Biostrings)
+
+combine.genomes <- function(input.folder, output.file, AA = FALSE) {
+    ref.seq.files <- list.files(input.folder, full.names = TRUE, recursive = TRUE, pattern = "\\.(fasta|fna)$")
+    for (file in ref.seq.files) {
+        message('Processing ', file)
+        # annotate names
+        if (AA) {
+            seq <- readAAStringSet(file)
+        } else {
+            seq <- readDNAStringSet(file)
+        }
+        writeXStringSet(seq, file, format = 'fasta', append = TRUE, compress = TRUE)
+    }
+}
+
+
+combine.genomes('/archive/yyang/project_data/downy/KrakenDB-contam-genome', '/data/run/yyang/project_data/downy/mmseqsDB/contam-genome.fasta.gz')
+combine.genomes('/archive/yyang/project_data/downy/ref-seq', '/data/run/yyang/project_data/downy/mmseqsDB/extract-genome.fasta.gz')
+
+combine.genomes('/archive/yyang/project_data/downy/KrakenDB-contam-protein', '/data/run/yyang/project_data/downy/mmseqsDB/contam-protein.fasta.gz', AA = TRUE)
+combine.genomes('/archive/yyang/project_data/downy/ref-seq-prot', '/data/run/yyang/project_data/downy/mmseqsDB/extract-protein.fasta.gz', AA = TRUE)
