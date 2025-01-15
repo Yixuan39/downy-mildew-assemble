d067efed158b3be6bc12dd95a1ab9febd916b053
Yixuan Yang <yyang55@ncsu.edu>
2025-01-15 10:42:22 -0500

    Update metaMDBG.sh

--- code/metaMDBG.sh
+++ code/metaMDBG.sh
@@ -2,11 +2,12 @@
 
 INPUT_FOLDER="/data/run/yyang/project_data/downy/data"
 RESULT_PATH="/data/run/yyang/project_data/downy/metaMDBG"
-FILES=$(ls ${INPUT_FOLDER} | grep .fasta | sed 's/.fasta.gz//g')
+FILES=$(ls ${INPUT_FOLDER} | grep .fasta.gz | sed 's/.fasta.gz//g')
 threads=32
 mkdir -p ${RESULT_PATH}
 
 for FILE in ${FILES}; do
+    echo "Processing ${FILE}"
     # Assemble the raw reads
     metaMDBG asm \
         --out-dir ${RESULT_PATH}/${FILE}.asm \
