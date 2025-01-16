0e4845cb7f3487f7cf269ecb7377bf988f01e61a
Yixuan Yang <yyang55@ncsu.edu>
2025-01-16 12:29:31 -0500

    Update run-mmseqs2.sh

--- code/run-mmseqs2.sh
+++ code/run-mmseqs2.sh
@@ -1,12 +1,7 @@
 
 # bash file that run all mmseqs scripts
 
-EVALUES=(1e-5 1e-10 1e-15 1e-20)
-
-for EVALUE in ${EVALUES}; do
-    echo "Running mmseqs search with evalue ${EVALUE}"
-    bash mmseqs2/asm-mmseq-contam-genome.sh ${EVALUE}
-    bash mmseqs2/asm-mmseq-contam-protein.sh ${EVALUE}
-    bash mmseqs2/asm-mmseq-extract-genome.sh ${EVALUE}
-    bash mmseqs2/asm-mmseq-extract-protein.sh ${EVALUE}
-done
+bash mmseqs2/asm-mmseq-contam-genome.sh 
+bash mmseqs2/asm-mmseq-contam-protein.sh 
+bash mmseqs2/asm-mmseq-extract-genome.sh 
+bash mmseqs2/asm-mmseq-extract-protein.sh 
