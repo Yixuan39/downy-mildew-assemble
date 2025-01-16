4dadb648870f036751dc808c196ed71439817614
Yixuan Yang <yyang55@ncsu.edu>
2025-01-15 23:08:33 -0500

    Update mmseqs-db.R

--- code/mmseqs-db.R
+++ code/mmseqs-db.R
@@ -1,17 +1,10 @@
-library(Biostrings)
 library(tidyverse)
 
 combine.genomes <- function(input.folder, output.file, AA = FALSE) {
     # Check if output file exists and remove it
     if (file.exists(output.file)) {file.remove(output.file)}
-    # Remove the extension if it is .gz
-    if (str_detect(output.file, 'gz')) {output.file <- fs::path_ext_remove(output.file)}
-    
     # Ensure the output directory exists
-    output.dir <- dirname(output.file)
-    if (!dir.exists(output.dir)) {
-        dir.create(output.dir, recursive = TRUE)
-    }
+    dir.create(dirname(output.file), recursive = TRUE, showWarnings = FALSE)
     
     # List relevant files
     ref.seq.files <- list.files(
@@ -22,16 +15,10 @@ combine.genomes <- function(input.folder, output.file, AA = FALSE) {
     ) %>% str_subset('.masked', negate = TRUE)
     
     # Stop if no files are found
-    if (length(ref.seq.files) == 0) {
-        stop("No .fasta or .fna files found in the input folder!")
-    }
-    message('Processing files: ')
-    for (file in ref.seq.files) {
-        message(str_c(file, collapse = '\n'))
-        if(AA) {seq <- readAAStringSet(file)} else {seq <- readDNAStringSet(file)}
-        writeXStringSet(seq, output.file, append = TRUE)
-    }
-    system(paste0('pigz ', output.file))
+    if (length(ref.seq.files) == 0) {stop("No .fasta or .fna files found in the input folder!")}
+    message('Processing files: ', str_c(ref.seq.files, collapse = '\n'))
+    # concate files
+    system(paste0('cat ', str_c(ref.seq.files, collapse = ' '), ' | pigz > ', output.file))
 }
 
 combine.genomes('/data/run/yyang/project_data/downy/KrakenDB-contam-genome', '/data/run/yyang/project_data/downy/mmseqsDB/contam-genome.fasta.gz')
