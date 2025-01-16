0ddd06b769b30077fca9e005daf93b1218d5d5c9
Yixuan Yang <yyang55@ncsu.edu>
2025-01-15 20:32:11 -0500

    Update mmseqs-db.R

--- code/mmseqs-db.R
+++ code/mmseqs-db.R
@@ -1,7 +1,8 @@
+library(Biostrings)
 library(stringr)
 
-
-combine.genomes <- function(input.folder, output.file) {
+combine.genomes <- function(input.folder, output.file, AA = FALSE) {
+    if (str_detect(output.file, 'gz')) {output.file <- fs::path_ext_remove(output.file)}
     # Check if output file exists and remove it
     if (file.exists(output.file)) {
         file.remove(output.file)
@@ -18,24 +19,26 @@ combine.genomes <- function(input.folder, output.file) {
         input.folder,
         full.names = TRUE,
         recursive = TRUE,
-        pattern = "\\.(fasta|fna)$"
+        pattern = "(fasta|fna)"
     )
     
     # Stop if no files are found
     if (length(ref.seq.files) == 0) {
         stop("No .fasta or .fna files found in the input folder!")
     }
-    message(str_c("processing ", ref.seq.files))
-    # Quote file paths to handle spaces and special characters
-    quoted_files <- str_c(shQuote(ref.seq.files), collapse = " ")
-    
-    # Build and execute the system command
-    command <- str_glue("cat {quoted_files} | gzip > {shQuote(output.file)}")
-    system(command)
+    message('Processing files: ')
+    message(str_c(ref.seq.files, collapse = '\n'))
+    # read in files parallel
+    genome.list <- lapply(ref.seq.files, function(file) {
+        if(AA) {readAAStringSet(file)} else {readDNAStringSet(file)}
+    })
+    genome.list <- DNAStringSetList(genome.list)
+    writeXStringSet(unlist(genome.list), output.file)
+    system(paste0('pigz ', output.file))
 }
-
-combine.genomes('/data/run/yyang/project_data/downy/KrakenDB-contam-genome', '/data/run/yyang/project_data/downy/mmseqsDB/contam-genome.fasta.gz')
-combine.genomes('/data/run/yyang/project_data/downy/ref-seq', '/data/run/yyang/project_data/downy/mmseqsDB/extract-genome.fasta.gz')
-
-combine.genomes('/data/run/yyang/project_data/downy/KrakenDB-contam-protein', '/data/run/yyang/project_data/downy/mmseqsDB/contam-protein.fasta.gz')
-combine.genomes('/data/run/yyang/project_data/downy/ref-seq-prot', '/data/run/yyang/project_data/downy/mmseqsDB/extract-protein.fasta.gz')
+combine.genomes('~/project_data/downy/oomycota-genome/', '~/project_data/downy/test/oomycota-genome.fasta.gz')
+# combine.genomes('/data/run/yyang/project_data/downy/KrakenDB-contam-genome', '/data/run/yyang/project_data/downy/mmseqsDB/contam-genome.fasta.gz')
+# combine.genomes('/data/run/yyang/project_data/downy/ref-seq', '/data/run/yyang/project_data/downy/mmseqsDB/extract-genome.fasta.gz')
+# 
+# combine.genomes('/data/run/yyang/project_data/downy/KrakenDB-contam-protein', '/data/run/yyang/project_data/downy/mmseqsDB/contam-protein.fasta.gz')
+# combine.genomes('/data/run/yyang/project_data/downy/ref-seq-prot', '/data/run/yyang/project_data/downy/mmseqsDB/extract-protein.fasta.gz')
