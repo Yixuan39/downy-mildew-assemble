89cc6ffd9cea6e81a4c0522154c951af19b050f8
Yixuan Yang <yyang55@ncsu.edu>
2025-01-17 14:47:51 -0500

    Update kraken2-db.sh

--- code/kraken2-db.sh
+++ code/kraken2-db.sh
@@ -1,20 +1,20 @@
 THREADS=32
 REF_PATH=/data/run/yyang/project_data/downy/ref-seq/
 # download kraken2 bacteria
-kraken2-build --download-library bacteria --db ${REF_PATH}/genome/bacteria --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library bacteria --db ${REF_PATH}/protein/bacteria --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
+kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
 # download kraken2 fungi
-kraken2-build --download-library fungi --db ${REF_PATH}/genome/fungi --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library fungi --db ${REF_PATH}/protein/fungi --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library fungi --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
+kraken2-build --download-library fungi --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
 # download kraken2 human
-kraken2-build --download-library human --db ${REF_PATH}/genome/human --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library human --db ${REF_PATH}/protein/human --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library human --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
+kraken2-build --download-library human --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
 
 # combine genome files
 FILES=$(find ${REF_PATH}/genome -name "*.fna")
 cat FILES | pigz > ${REF_PATH}/genome-bfh.fna.gz
-rm -rf ${REF_PATH}/genome
+# rm -rf ${REF_PATH}/genome
 # combine protein files
 FILES=$(find ${REF_PATH}/protein -name "*.faa")
 cat FILES | pigz > ${REF_PATH}/protein-bfh.faa.gz
-rm -rf ${REF_PATH}/protein
+# rm -rf ${REF_PATH}/protein
