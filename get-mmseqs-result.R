67b4f9bcc39a095a9e937b35b7ccb83833e732cf
Yixuan Yang <yyang55@ncsu.edu>
2025-01-20 15:02:59 -0500

    Create get-mmseqs-result.R

---
+++ get-mmseqs-result.R
@@ -0,0 +1,26 @@
+library(data.table)
+library(Biostrings)
+library(fs)
+
+path <- '/data/run/yyang/project_data/downy/mmseqs_result/'
+# set evalue
+evalues <- c(1e-5, 1e-10, 1e-15, 1e-20)
+
+files <- list.files(path = args$input, full.names = TRUE, recursive = TRUE, pattern = '.tsv')
+if (length(files) == 0) {stop('No .tsv files found in the input folder!')}
+
+for (ev in evalues) {
+    for (file in files) {
+        message('Processing ', file)
+        # read tsv file
+        data <- fread(file)
+        # filter by evalue
+        data <- data[evalue <= ev]
+        seq <- DNAStringSet(data$qseq)
+        names(seq) <- data$query
+        # write to fasta
+        out.path <- file.path(dirname(file), ev)
+        if (!dir.exists(out.path)) {dir.create(out.path, recursive = TRUE)}
+        writeXStringSet(seq, file.path(out.path, paste0(fs::path_ext_remove(basename(file)), '.fasta')), format = 'fasta', compress = TRUE)
+    }
+}
