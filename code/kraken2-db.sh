16696cd12a7e01ab5d51aca611abfdc055a915d3
Yixuan Yang <yyang55@ncsu.edu>
2025-01-20 10:57:46 -0500

    combine kraken db building in one script

--- code/kraken2-db.sh
+++ code/kraken2-db.sh
@@ -1,20 +1,52 @@
 THREADS=32
 REF_PATH=/data/run/yyang/project_data/downy/ref-seq/
 # download kraken2 bacteria
-kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
 # download kraken2 fungi
-kraken2-build --download-library fungi --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library fungi --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library fungi --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+kraken2-build --download-library fungi --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
 # download kraken2 human
-kraken2-build --download-library human --db ${REF_PATH}/genome --threads ${THREADS} --use-ftp --no-masking
-kraken2-build --download-library human --db ${REF_PATH}/protein --threads ${THREADS} --use-ftp --no-masking --protein
+kraken2-build --download-library human --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+kraken2-build --download-library human --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
 
 # combine genome files
 FILES=$(find ${REF_PATH}/genome -name "*.fna")
-cat FILES | pigz > ${REF_PATH}/genome-bfh.fna.gz
+cat ${FILES} > ${REF_PATH}/genome-bfh.fasta
 # rm -rf ${REF_PATH}/genome
 # combine protein files
 FILES=$(find ${REF_PATH}/protein -name "*.faa")
-cat FILES | pigz > ${REF_PATH}/protein-bfh.faa.gz
+cat ${FILES} > ${REF_PATH}/protein-bfh.fasta
 # rm -rf ${REF_PATH}/protein
+
+# build kraken2 oomycota db
+DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/oomycota-genome
+kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/genome-bfh.fasta --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/oomycota-genome.fasta --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --build --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --clean --threads ${THREADS}
+
+# build kraken2 oomycota protein db
+DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/oomycota-protein
+kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/protein-bfh.fasta --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/oomycota-protein.fasta --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --build --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --clean --threads ${THREADS} --protein
+
+# build kraken2 contam db
+DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/contam-genome
+kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/genome-bfh.fasta --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/contam-genome.fasta --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --build --threads ${THREADS}
+kraken2-build --db ${DB_PATH} --clean --threads ${THREADS}
+
+# build kraken2 contam protein db
+DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/contam-protein
+kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/protein-bfh.fasta --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/contam-protein.fasta --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --build --threads ${THREADS} --protein
+kraken2-build --db ${DB_PATH} --clean --threads ${THREADS} --protein
