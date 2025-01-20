c9d77911da3372d1b69f5d55edbf2a7961397edc
Yixuan Yang <yyang55@ncsu.edu>
2025-01-20 12:31:02 -0500

    update mmseqs method

---
+++ code/mmseqs-assemble.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+INPUT_FOLDER=/data/run/yyang/project_data/downy/metaMDBG
+FILES=$(ls ${INPUT_FOLDER}/*.fastq.gz 2>/dev/null | xargs -n 1 basename)
+
+# assemble sequence first, then extract oomycota sequence use genome search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-genome.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/oomycota-genome
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.tsv \
+    tmp \
+    -e 10 \
+    --max-seqs 1 \
+    --search-type 3 \
+    --format-mode 4 \
+    --format-output query,qseq,evalue
+done
+
+# assemble sequence first, then extract oomycota sequence use protein search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-protein.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/oomycota-protein
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.tsv \
+    tmp \
+    -e 10 \
+    --max-seqs 1 \
+    --search-type 2 \
+    --translation-mode 1 \
+    --format-mode 4 \
+    --format-output query,qseq,evalue
+done
+
+# assemble sequence first, then extract oomycota sequence use genome search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/contam-genome.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/contam-genome
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.tsv \
+    tmp \
+    -e 10 \
+    --max-seqs 1 \
+    --search-type 3 \
+    --format-mode 4 \
+    --format-output query,qseq,evalue
+done
+
+# assemble sequence first, then extract oomycota sequence use protein search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/contam-protein.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/contam-protein
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.tsv \
+    tmp \
+    -e 10 \
+    --max-seqs 1 \
+    --search-type 2 \
+    --translation-mode 1 \
+    --format-mode 4 \
+    --format-output query,qseq,evalue
+done
--- code/run-mmseqs2.sh
+++
@@ -1,7 +0,0 @@
-
-# bash file that run all mmseqs scripts
-
-bash mmseqs2/asm-mmseq-contam-genome.sh 
-bash mmseqs2/asm-mmseq-contam-protein.sh 
-bash mmseqs2/asm-mmseq-extract-genome.sh 
-bash mmseqs2/asm-mmseq-extract-protein.sh 
