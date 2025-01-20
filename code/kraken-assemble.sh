e8acac41ad96e82983cec11fdfa68bad29afec34
Yixuan Yang <yyang55@ncsu.edu>
2025-01-20 10:57:20 -0500

    combine kraken runs in one script

---
+++ code/kraken-assemble.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+
+CS=$1
+threads=32
+
+# Use metaMDBG assemble pacbio
+
+INPUT_FOLDER=/data/run/yyang/project_data/downy/metaMDBG
+FILES=$(ls ${INPUT_FOLDER}/*.fastq.gz 2>/dev/null | xargs -n 1 basename)
+
+# assemble sequence first, then extract oomycota sequence use genome search
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/oomycota-genome
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/assemble-classify-oomycota-genome/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw assembly
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}.fasta
+    # extract sequences classified as Oomycota (taxid 4762)
+    extract_kraken_reads.py \
+        -k ${RESULT_PATH}/${FILE}.kraken \
+        -s ${INPUT_FOLDER}/${FILE}.fasta \
+        --taxid 4762 \
+        --output ${RESULT_PATH}/${FILE}.fasta \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --include-children
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+done
+
+# assemble sequence first, then extract oomycota sequence use protein search
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/oomycota-protein
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/assemble-classify-oomycota-protein/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw assembly
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}.fasta
+    # extract sequences classified as Oomycota (taxid 4762)
+    extract_kraken_reads.py \
+        -k ${RESULT_PATH}/${FILE}.kraken \
+        -s ${INPUT_FOLDER}/${FILE}.fasta \
+        --taxid 4762 \
+        --output ${RESULT_PATH}/${FILE}.fasta \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --include-children
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+done
+
+# assemble sequence first, then remove contamination sequence use genome search.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/contam-genome
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/assemble-classify-contam-genome/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --unclassified-out ${RESULT_PATH}/${FILE} \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+done
+
+# assemble sequence first, then remove contamination sequence use protein search.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/contam-protein
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/assemble-classify-contam-protein/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --unclassified-out ${RESULT_PATH}/${FILE}.fasta \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}.fasta
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+done
+
+
+INPUT_FOLDER=/data/run/yyang/project_data/downy/data
+FILES=$(ls ${INPUT_FOLDER}/*.fastq.gz 2>/dev/null | xargs -n 1 basename)
+
+# extract oomycota sequence with kraken2 using genome search, then assemble the genome.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/oomycota-genome
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/classify-assemble-oomycota-genome/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}
+    # extract sequences classified as Oomycota (taxid 4762)
+    extract_kraken_reads.py \
+        -k ${RESULT_PATH}/${FILE}.kraken \
+        -s ${INPUT_FOLDER}/${FILE} \
+        --taxid 4762 \
+        --output ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --include-children \
+        --fastq-output
+    # Assemble the extracted reads
+    metaMDBG asm \
+        --out-dir ${RESULT_PATH}/${FILE}_asm \
+        --in-hifi ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --threads ${threads}
+    mv ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz ${RESULT_PATH}/${FILE}
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+    rm ${RESULT_PATH}/${FILE}.tmp.fastq
+    rm -rf ${RESULT_PATH}/${FILE}_asm
+done
+
+
+# extract oomycota sequence with kraken2 using protein search, then assemble the genome.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/oomycota-protein
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/classify-assemble-oomycota-protein/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}
+    # extract sequences classified as Oomycota (taxid 4762)
+    extract_kraken_reads.py \
+        -k ${RESULT_PATH}/${FILE}.kraken \
+        -s ${INPUT_FOLDER}/${FILE} \
+        --taxid 4762 \
+        --output ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --include-children \
+        --fastq-output
+    # Assemble the extracted reads
+    metaMDBG asm \
+        --out-dir ${RESULT_PATH}/${FILE}_asm \
+        --in-hifi ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --threads ${threads}
+    mv ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz ${RESULT_PATH}/${FILE}
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+    rm ${RESULT_PATH}/${FILE}.tmp.fastq
+    rm -rf ${RESULT_PATH}/${FILE}_asm
+done
+
+# exclude the contamination sequences first with kraken2 using genome search, then assemble the genome.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/contam-genome
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/classify-assemble-contam-genome/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --unclassified-out ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}
+    # Assemble the extracted reads
+    metaMDBG asm \
+        --out-dir ${RESULT_PATH}/${FILE}_asm \
+        --in-hifi ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --threads ${threads}
+    mv ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz ${RESULT_PATH}/${FILE}
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+    rm ${RESULT_PATH}/${FILE}.tmp.fastq
+    rm -rf ${RESULT_PATH}/${FILE}_asm
+done
+
+# exclude the contamination sequences first with kraken2 using protein search, then assemble the genome.
+
+KrakenDB=/data/run/yyang/project_data/downy/KrakenDB/contam-protein
+RESULT_PATH=/data/run/yyang/project_data/downy/Kraken-result/classify-assemble-contam-protein/${CS}
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # Run kraken2 on raw reads
+    kraken2 --db ${KrakenDB} \
+        --threads ${threads} \
+        --output ${RESULT_PATH}/${FILE}.kraken \
+        --report ${RESULT_PATH}/${FILE}.kreport \
+        --unclassified-out ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --confidence ${CS} \
+        ${INPUT_FOLDER}/${FILE}
+    # Assemble the extracted reads
+    metaMDBG asm \
+        --out-dir ${RESULT_PATH}/${FILE}_asm \
+        --in-hifi ${RESULT_PATH}/${FILE}.tmp.fastq \
+        --threads ${threads}
+    mv ${RESULT_PATH}/${FILE}_asm/contigs.fasta.gz ${RESULT_PATH}/${FILE}
+    # remove unnecessary files
+    rm ${RESULT_PATH}/${FILE}.kraken
+    rm ${RESULT_PATH}/${FILE}.tmp.fastq
+    rm -rf ${RESULT_PATH}/${FILE}_asm
+done
