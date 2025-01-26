64c7fd63760c8a0fa83286ca3152bdcbdff9b5f4
Yixuan Yang <yyang55@ncsu.edu>
2025-01-26 00:21:07 -0500

    Update mmseqs-assemble.sh

--- code/mmseqs-assemble.sh
+++ code/mmseqs-assemble.sh
@@ -2,132 +2,6 @@
 
 EV=$1
 threads=32
-INPUT_FOLDER=/data/run/yyang/project_data/downy/metaMDBG
-FILES=$(ls ${INPUT_FOLDER}/*.fasta.gz 2>/dev/null | xargs -n 1 basename)
-
-# assemble sequence first, then extract oomycota sequence use genome search
-
-DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-genome.fasta.gz
-RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-oomycota-genome
-mkdir -p ${RESULT_PATH}
-
-for FILE in ${FILES}; do
-    # run mmseqs2 on the assembly
-    mmseqs easy-search \
-    ${INPUT_FOLDER}/${FILE} \
-    ${DB} \
-    ${RESULT_PATH}/${FILE}.txt \
-    ${RESULT_PATH}/tmp \
-    -e ${EV} \
-    --max-accept 1 \
-    --search-type 3 \
-    --format-mode 4 \
-    --format-output query
-    
-    seqkit grep \
-    --threads ${threads} \
-    --pattern-file ${RESULT_PATH}/${FILE}.txt \
-    --out-file ${RESULT_PATH}/${EV}/${FILE} \
-    ${INPUT_FOLDER}/${FILE} 
-    
-    rm ${RESULT_PATH}/${FILE}.txt
-    rm -rf ${RESULT_PATH}/tmp
-done
-
-# assemble sequence first, then extract oomycota sequence use protein search
-
-DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-protein.fasta.gz
-RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-oomycota-protein
-mkdir -p ${RESULT_PATH}
-
-for FILE in ${FILES}; do
-    # run mmseqs2 on the assembly
-    mmseqs easy-search \
-    ${INPUT_FOLDER}/${FILE} \
-    ${DB} \
-    ${RESULT_PATH}/${FILE}.txt \
-    ${RESULT_PATH}/tmp \
-    -e ${EV} \
-    --max-accept 1 \
-    --search-type 2 \
-    --translation-mode 1 \
-    --format-mode 4 \
-    --format-output query
-    
-    seqkit grep \
-    --threads ${threads} \
-    --pattern-file ${RESULT_PATH}/${FILE}.txt \
-    --out-file ${RESULT_PATH}/${EV}/${FILE} \
-    ${INPUT_FOLDER}/${FILE} 
-    
-    rm ${RESULT_PATH}/${FILE}.txt     
-    rm -rf ${RESULT_PATH}/tmp
-done
-
-# assemble sequence first, then remove contamination sequence use genome search
-cat /data/run/yyang/project_data/downy/ref-seq/contam-genome.fasta.gz \
-    /data/run/yyang/project_data/downy/ref-seq/genome-bfh.fasta.gz > /data/run/yyang/project_data/downy/ref-seq/contam-large-genome.fasta.gz
-DB=/data/run/yyang/project_data/downy/ref-seq/contam-large-genome.fasta.gz
-RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-contam-genome
-mkdir -p ${RESULT_PATH}
-
-for FILE in ${FILES}; do
-    # run mmseqs2 on the assembly
-    mmseqs easy-search \
-    ${INPUT_FOLDER}/${FILE} \
-    ${DB} \
-    ${RESULT_PATH}/${FILE}.txt \
-    ${RESULT_PATH}/tmp \
-    -e ${EV} \
-    --max-accept 1 \
-    --search-type 3 \
-    --format-mode 4 \
-    --format-output query
-    
-    seqkit grep \
-    --invert-match \
-    --threads ${threads} \
-    --pattern-file ${RESULT_PATH}/${FILE}.txt \
-    --out-file ${RESULT_PATH}/${EV}/${FILE} \
-    ${INPUT_FOLDER}/${FILE} 
-    
-    rm ${RESULT_PATH}/${FILE}.txt     
-    rm -rf ${RESULT_PATH}/tmp
-done
-
-# assemble sequence first, then remove contamination sequence use protein search
-cat /data/run/yyang/project_data/downy/ref-seq/contam-protein.fasta.gz \
-    /data/run/yyang/project_data/downy/ref-seq/protein-bfh.fasta.gz > /data/run/yyang/project_data/downy/ref-seq/contam-large-protein.fasta.gz
-DB=/data/run/yyang/project_data/downy/ref-seq/contam-large-protein.fasta.gz
-RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-contam-protein
-mkdir -p ${RESULT_PATH}
-
-for FILE in ${FILES}; do
-    # run mmseqs2 on the assembly
-    mmseqs easy-search \
-    ${INPUT_FOLDER}/${FILE} \
-    ${DB} \
-    ${RESULT_PATH}/${FILE}.txt \
-    ${RESULT_PATH}/tmp \
-    -e ${EV} \
-    --max-accept 1 \
-    --search-type 2 \
-    --translation-mode 1 \
-    --format-mode 4 \
-    --format-output query
-    
-    seqkit grep \
-    --invert-match \
-    --threads ${threads} \
-    --pattern-file ${RESULT_PATH}/${FILE}.txt \
-    --out-file ${RESULT_PATH}/${EV}/${FILE} \
-    ${INPUT_FOLDER}/${FILE} 
-    
-    rm ${RESULT_PATH}/${FILE}.txt     
-    rm -rf ${RESULT_PATH}/tmp
-done
-
-
 
 
 ###########################################################################
@@ -288,5 +162,140 @@ for FILE in ${FILES}; do
     rm ${RESULT_PATH}/${FILE}.txt     
     rm -rf ${RESULT_PATH}/tmp
 done
+
+
+
+###########################################################################
+
+
+INPUT_FOLDER=/data/run/yyang/project_data/downy/metaMDBG
+FILES=$(ls ${INPUT_FOLDER}/*.fasta.gz 2>/dev/null | xargs -n 1 basename)
+
+# assemble sequence first, then extract oomycota sequence use genome search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-genome.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-oomycota-genome
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.txt \
+    ${RESULT_PATH}/tmp \
+    -e ${EV} \
+    --max-accept 1 \
+    --search-type 3 \
+    --format-mode 4 \
+    --format-output query
+    
+    seqkit grep \
+    --threads ${threads} \
+    --pattern-file ${RESULT_PATH}/${FILE}.txt \
+    --out-file ${RESULT_PATH}/${EV}/${FILE} \
+    ${INPUT_FOLDER}/${FILE} 
+    
+    rm ${RESULT_PATH}/${FILE}.txt
+    rm -rf ${RESULT_PATH}/tmp
+done
+
+# assemble sequence first, then extract oomycota sequence use protein search
+
+DB=/data/run/yyang/project_data/downy/ref-seq/oomycota-protein.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-oomycota-protein
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.txt \
+    ${RESULT_PATH}/tmp \
+    -e ${EV} \
+    --max-accept 1 \
+    --search-type 2 \
+    --translation-mode 1 \
+    --format-mode 4 \
+    --format-output query
+    
+    seqkit grep \
+    --threads ${threads} \
+    --pattern-file ${RESULT_PATH}/${FILE}.txt \
+    --out-file ${RESULT_PATH}/${EV}/${FILE} \
+    ${INPUT_FOLDER}/${FILE} 
+    
+    rm ${RESULT_PATH}/${FILE}.txt     
+    rm -rf ${RESULT_PATH}/tmp
+done
+
+# assemble sequence first, then remove contamination sequence use genome search
+cat /data/run/yyang/project_data/downy/ref-seq/contam-genome.fasta.gz \
+    /data/run/yyang/project_data/downy/ref-seq/genome-bfh.fasta.gz > /data/run/yyang/project_data/downy/ref-seq/contam-large-genome.fasta.gz
+DB=/data/run/yyang/project_data/downy/ref-seq/contam-large-genome.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-contam-genome
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.txt \
+    ${RESULT_PATH}/tmp \
+    -e ${EV} \
+    --max-accept 1 \
+    --search-type 3 \
+    --format-mode 4 \
+    --format-output query
+    
+    seqkit grep \
+    --invert-match \
+    --threads ${threads} \
+    --pattern-file ${RESULT_PATH}/${FILE}.txt \
+    --out-file ${RESULT_PATH}/${EV}/${FILE} \
+    ${INPUT_FOLDER}/${FILE} 
+    
+    rm ${RESULT_PATH}/${FILE}.txt     
+    rm -rf ${RESULT_PATH}/tmp
+done
+
+# assemble sequence first, then remove contamination sequence use protein search
+cat /data/run/yyang/project_data/downy/ref-seq/contam-protein.fasta.gz \
+    /data/run/yyang/project_data/downy/ref-seq/protein-bfh.fasta.gz > /data/run/yyang/project_data/downy/ref-seq/contam-large-protein.fasta.gz
+DB=/data/run/yyang/project_data/downy/ref-seq/contam-large-protein.fasta.gz
+RESULT_PATH=/data/run/yyang/project_data/downy/mmseqs_result/assemble-classify-contam-protein
+mkdir -p ${RESULT_PATH}
+
+for FILE in ${FILES}; do
+    # run mmseqs2 on the assembly
+    mmseqs easy-search \
+    ${INPUT_FOLDER}/${FILE} \
+    ${DB} \
+    ${RESULT_PATH}/${FILE}.txt \
+    ${RESULT_PATH}/tmp \
+    -e ${EV} \
+    --max-accept 1 \
+    --search-type 2 \
+    --translation-mode 1 \
+    --format-mode 4 \
+    --format-output query
+    
+    seqkit grep \
+    --invert-match \
+    --threads ${threads} \
+    --pattern-file ${RESULT_PATH}/${FILE}.txt \
+    --out-file ${RESULT_PATH}/${EV}/${FILE} \
+    ${INPUT_FOLDER}/${FILE} 
+    
+    rm ${RESULT_PATH}/${FILE}.txt     
+    rm -rf ${RESULT_PATH}/tmp
+done
+
+
+
+
+
 
 
