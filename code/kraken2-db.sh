b64da3f2f6480e5a0e52867949e70b0daf01461a
Yixuan39 <71982982+Yixuan39@users.noreply.github.com>
2025-02-05 13:01:22 -0500

    don't download in script

--- code/kraken2-db.sh
+++ code/kraken2-db.sh
@@ -2,24 +2,24 @@
 
 THREADS=32
 REF_PATH=/data/run/yyang/project_data/downy/ref-seq/
-# download kraken2 bacteria
-kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
-kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
-# download kraken2 fungi
-kraken2-build --download-library fungi --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
-kraken2-build --download-library fungi --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
-# download kraken2 human
-kraken2-build --download-library human --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
-kraken2-build --download-library human --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
-
-# combine genome files
-FILES=$(find ${REF_PATH}/genome -name "*.fna")
-cat ${FILES} > ${REF_PATH}/genome-bfh.fasta
-# rm -rf ${REF_PATH}/genome
-# combine protein files
-FILES=$(find ${REF_PATH}/protein -name "*.faa")
-cat ${FILES} > ${REF_PATH}/protein-bfh.fasta
-# rm -rf ${REF_PATH}/protein
+# # download kraken2 bacteria
+# kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+# kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
+# # download kraken2 fungi
+# kraken2-build --download-library fungi --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+# kraken2-build --download-library fungi --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
+# # download kraken2 human
+# kraken2-build --download-library human --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
+# kraken2-build --download-library human --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
+# 
+# # combine genome files
+# FILES=$(find ${REF_PATH}/genome -name "*.fna")
+# cat ${FILES} > ${REF_PATH}/genome-bfh.fasta
+# # rm -rf ${REF_PATH}/genome
+# # combine protein files
+# FILES=$(find ${REF_PATH}/protein -name "*.faa")
+# cat ${FILES} > ${REF_PATH}/protein-bfh.fasta
+# # rm -rf ${REF_PATH}/protein
 
 # build kraken2 oomycota db
 DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/oomycota-genome
