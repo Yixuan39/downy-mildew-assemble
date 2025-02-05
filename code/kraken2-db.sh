ef8482e1a0b287b985176e098908fe232ab63a68
Yixuan39 <71982982+Yixuan39@users.noreply.github.com>
2025-02-05 11:14:49 -0500

    change home dir

--- code/kraken2-db.sh
+++ code/kraken2-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 THREADS=32
-REF_PATH=/data/run/yyang/project_data/downy/ref-seq/
+REF_PATH=$HOME/project_data/downy/ref-seq/
 # download kraken2 bacteria
 kraken2-build --download-library bacteria --db ${REF_PATH}/genome --threads ${THREADS} --no-masking
 kraken2-build --download-library bacteria --db ${REF_PATH}/protein --threads ${THREADS} --no-masking --protein
@@ -22,7 +22,7 @@ cat ${FILES} > ${REF_PATH}/protein-bfh.fasta
 # rm -rf ${REF_PATH}/protein
 
 # build kraken2 oomycota db
-DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/oomycota-genome
+DB_PATH=$HOME/project_data/downy/KrakenDB/oomycota-genome
 kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/genome-bfh.fasta --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/oomycota-genome.fasta --threads ${THREADS}
@@ -31,7 +31,7 @@ kraken2-build --db ${DB_PATH} --build --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --clean --threads ${THREADS}
 
 # build kraken2 oomycota protein db
-DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/oomycota-protein
+DB_PATH=$HOME/project_data/downy/KrakenDB/oomycota-protein
 kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS} 
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/protein-bfh.fasta --threads ${THREADS} --protein
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/oomycota-protein.fasta --threads ${THREADS} --protein
@@ -40,7 +40,7 @@ kraken2-build --db ${DB_PATH} --build --threads ${THREADS} --protein
 kraken2-build --db ${DB_PATH} --clean --threads ${THREADS} --protein
 
 # build kraken2 contam db
-DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/contam-genome
+DB_PATH=$HOME/project_data/downy/KrakenDB/contam-genome
 kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/genome-bfh.fasta --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/contam-genome.fasta --threads ${THREADS}
@@ -48,7 +48,7 @@ kraken2-build --db ${DB_PATH} --build --threads ${THREADS}
 kraken2-build --db ${DB_PATH} --clean --threads ${THREADS}
 
 # build kraken2 contam protein db
-DB_PATH=/data/run/yyang/project_data/downy/KrakenDB/contam-protein
+DB_PATH=$HOME/project_data/downy/KrakenDB/contam-protein
 kraken2-build --db ${DB_PATH} --download-taxonomy --threads ${THREADS} 
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/protein-bfh.fasta --threads ${THREADS} --protein
 kraken2-build --db ${DB_PATH} --add-to-library ${REF_PATH}/contam-protein.fasta --threads ${THREADS} --protein
