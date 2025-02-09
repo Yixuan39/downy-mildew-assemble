924f50d648135422e7b21c5a8cbb2598fc9b8879
Yixuan Yang <yyang55@ncsu.edu>
2025-02-09 12:15:44 -0500

    Create fcs_gx.sh

---
+++ code/fcs_gx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# input folder argument
+input_folder=$1
+GX_DB=$HOME/project_data/downy/fcs-db/
+# get the fasta file full name
+files=$(find ${input_folder} -name "*.fasta.gz")
+dirs=$(dirname ${files} | sort | uniq)
+
+for dir in ${dirs}; do
+    out_dir=${dir}/verification
+    mkdir -p ${out_dir}
+    echo "Processing ${dir}"
+    files=$(find ${dir} -name "*.fasta.gz")
+    for file in ${files}; do
+        run_gx.py --fasta ${file} \
+                  --tax-id 4762 \
+                  --gx-db ${GX_DB} \
+                  --out-dir ${out_dir} \
+                  --out-basename $(basename ${file%.fasta.gz}) 
+    done
+done
