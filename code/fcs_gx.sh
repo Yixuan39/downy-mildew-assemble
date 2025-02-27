8c54e72c4e7022ca373b2de72e4a4a2320ae627e
Yixuan Yang <yyang55@ncsu.edu>
2025-02-27 17:05:10 -0500

    screen and clean genome with fcs gx

--- code/fcs_gx.sh
+++ code/fcs_gx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # input folder argument
-export GX_NUM_CORES=32
+export GX_NUM_CORES=24
 input_folder=$HOME/project_data/downy/Kraken-result/
 GX_DB=$HOME/project_data/downy/fcs-db/
 # get the fasta file full name
@@ -14,11 +14,17 @@ for dir in ${dirs}; do
     echo "Processing ${dir}"
     files=$(find ${dir} -name "*.fasta.gz")
     for file in ${files}; do
+        # check contamination in the genome, 4762 is the tax id for oomycota.
         run_gx.py --fasta ${file} \
                   --tax-id 4762 \
                   --gx-db ${GX_DB} \
                   --out-dir ${out_dir} \
                   --out-basename $(basename ${file%.fasta.gz}) 
+        # exclude contam read...
+        gx clean-genome \
+                  --input ${file} \
+                  --action-report ${out_dir}/$(basename ${file%.fasta.gz}).fcs_gx_report.txt \
+                  --output ${out_dir}/$(basename ${file})
     done
 done
 
