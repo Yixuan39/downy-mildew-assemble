1a6590ff8f7c035134a0e0592e4aed70868c8211
Yixuan Yang <yyang55@ncsu.edu>
2025-02-27 19:32:06 -0500

    Update test.sh

--- code/mmseqs2/test.sh
+++ code/mmseqs2/test.sh
@@ -13,5 +13,5 @@ threads=24
 INPUT_FOLDER=$HOME/project_data/downy/data
 FILES=$(ls ${INPUT_FOLDER}/*.fastq.gz 2>/dev/null | xargs -n 1 basename)
 FILE=${FILES[$SLURM_ARRAY_TASK_ID - 1]}
-echo FILES
-echo FILE
+echo $FILES
+echo $FILE
