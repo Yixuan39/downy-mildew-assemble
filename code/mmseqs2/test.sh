cdbfc5758e72e38c3221cc12559ffc2a256ea11f
Yixuan Yang <yyang55@ncsu.edu>
2025-02-27 19:31:01 -0500

    Create test.sh

---
+++ code/mmseqs2/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --array=1-3
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=0
+
+EV=1e-10
+threads=24
+
+###########################################################################
+# classify sequence first, then assemble with metaMDBG                    #
+###########################################################################
+
+INPUT_FOLDER=$HOME/project_data/downy/data
+FILES=$(ls ${INPUT_FOLDER}/*.fastq.gz 2>/dev/null | xargs -n 1 basename)
+FILE=${FILES[$SLURM_ARRAY_TASK_ID - 1]}
+echo FILES
+echo FILE
