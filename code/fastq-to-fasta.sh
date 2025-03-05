7439d5fcbc6f528a9e79b7f12479c7373a20b7b4
Yixuan Yang <yyang55@ncsu.edu>
2025-03-05 14:33:32 -0500

    Create fastq-to-fasta.sh

---
+++ code/fastq-to-fasta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+INPUT_FOLDER=$HOME/project_data/downy/data
+FILES=$(find ${INPUT_FOLDER} -name "*.fastq.gz")
+OUTPUT_FOLDER=$HOME/project_data/downy/data-fasta
+
+for FILE in $FILES
+do
+    echo "Processing file $FILE"
+    FILENAME=$(basename $FILE)
+    FILENAME=${FILENAME%.fastq.gz}
+    OUTPUT_FILE=$OUTPUT_FOLDER/$FILENAME.fasta
+    seqtk seq -A $FILE > $OUTPUT_FILE
+    gzip $OUTPUT_FILE
+done
