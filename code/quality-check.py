380d1714cbe640a9f5d015743a4876a8f0f735ba
Yixuan Yang <yyang55@ncsu.edu>
2025-03-04 21:18:00 -0500

    Update quality-check.py

--- code/quality-check.py
+++ code/quality-check.py
@@ -4,7 +4,7 @@ import argparse
 import shutil
 import pandas
 
-def compleasm(input_file, output_dir, linkage, threads=24, library_path='$HOME/project_data/downy/BUSCO_DB'):
+def compleasm(input_file, output_dir, threads, library_path, linkage):
     output_dir = os.path.join(output_dir, linkage)
     os.makedirs(output_dir, exist_ok=True)
     cmd = 'compleasm.py run \
@@ -12,7 +12,7 @@ def compleasm(input_file, output_dir, linkage, threads=24, library_path='$HOME/p
            --output_dir {} \
            --threads {} \
            --library_path {} \
-           --lineage {}'.format(input_file, output_dir, linkage, threads=24, library_path='$HOME/project_data/downy/BUSCO_DB')
+           --lineage {}'.format(input_file, output_dir, threads, library_path, linkage)
     subprocess.call(cmd, shell=True)
     # read in the output
     output_file = os.path.join(output_dir, 'summary.txt')
@@ -20,7 +20,7 @@ def compleasm(input_file, output_dir, linkage, threads=24, library_path='$HOME/p
     shutil.rmtree(output_dir, ignore_errors=False)
     return df
 
-def quast(input_file, output_dir, threads=24):
+def quast(input_file, output_dir, threads):
     output_dir = os.path.join(output_dir, 'quast')
     os.makedirs(output_dir, exist_ok=True)
     cmd = 'quast.py \
@@ -37,10 +37,10 @@ def quast(input_file, output_dir, threads=24):
   
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Quality check')
-    parser.add_argument('input_file', help='Input file in fasta format')
-    parser.add_argument('output_dir', help='Output directory')
-    parser.add_argument('threads', help='Number of threads', default=24)
-    parser.add_argument('library_path', help='path to compleasm library', default='$HOME/project_data/downy/BUSCO_DB')
+    parser.add_argument('--input_file', help='Input file in fasta format')
+    parser.add_argument('--output_dir', help='Output directory')
+    parser.add_argument('--threads', help='Number of threads', type=int, default=24)
+    parser.add_argument('--library_path', help='path to compleasm library', default='$HOME/project_data/downy/BUSCO_DB')
     args = parser.parse_args()
     compleasm_euk = compleasm(args.input_file, args.output_dir, args.threads, args.library_path, 'eukaryota_odb10')
     compleasm_stramenopiles = compleasm(args.input_file, args.output_dir, args.threads, args.library_path, 'stramenopiles_odb10')
