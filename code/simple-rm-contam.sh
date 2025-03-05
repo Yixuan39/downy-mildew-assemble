275366a52efa0d7666cbfddcacb0119ecebe763b
Yixuan Yang <yyang55@ncsu.edu>
2025-03-05 13:48:04 -0500

    Update simple-rm-contam.sh

--- code/simple-rm-contam.sh
+++ code/simple-rm-contam.sh
@@ -6,11 +6,9 @@ FILES=$(find ${INPUT_FOLDER} -name "*.fasta.gz")
 RESULT_PATH=$HOME/project_data/downy/simple-rm-contam-raw/
 GX_DB=$HOME/project_data/downy/fcs-db/
 BUSCO_DB=$HOME/project_data/downy/BUSCO_DB
-export GX_NUM_CORES=THREADS
+export GX_NUM_CORES=$THREADS
 
 for file in ${FILES}; do
-    out_dir=${RESULT_PATH}/$(basename ${file%.fasta.gz})
-    mkdir -p ${out_dir}
     # check contamination in the genome, 4762 is the tax id for oomycota.
     run_gx.py --fasta ${file} \
               --tax-id 4762 \
@@ -22,20 +20,24 @@ for file in ${FILES}; do
               --input ${file} \
               --action-report ${RESULT_PATH}/$(basename ${file%.fasta.gz}).fcs_gx_report.txt \
               --output ${RESULT_PATH}/$(basename ${file%.gz})
+    gzip ${RESULT_PATH}/$(basename ${file%.gz})
+    # get quality report
     python quality_check.py \
-              --input_file ${RESULT_PATH}/$(basename ${file%.gz}) \
-              --output_dir ${RESULT_PATH}/$(basename ${file%.gz}) \
+              --input_file ${RESULT_PATH}/$(basename ${file}) \
+              --output_dir ${RESULT_PATH} \
+              --suffix $(basename ${file%.fasta.gz})
               --library_path ${BUSCO_DB} \
               --threads ${THREADS}
 done
 
 INPUT_FOLDER=$HOME/project_data/downy/data
-FILES=$(find ${INPUT_FOLDER} -name "*.fasta.gz")
+FILES=$(find ${INPUT_FOLDER} -name "*.fastq.gz")
 RESULT_PATH=$HOME/project_data/downy/simple-rm-contam-asm/
 
 for file in ${FILES}; do
-    out_dir=${RESULT_PATH}/$(basename ${file%.fasta.gz})
-    mkdir -p ${out_dir}
+    # convert fastq to fasta
+    seqtk seq -A ${file} > ${file%.fastq.gz}.fasta.gz
+    file = ${file%.fastq.gz}.fasta.gz
     # check contamination in the genome, 4762 is the tax id for oomycota.
     run_gx.py --fasta ${file} \
               --tax-id 4762 \
@@ -47,9 +49,12 @@ for file in ${FILES}; do
               --input ${file} \
               --action-report ${RESULT_PATH}/$(basename ${file%.fasta.gz}).fcs_gx_report.txt \
               --output ${RESULT_PATH}/$(basename ${file%.gz})
+    gzip ${RESULT_PATH}/$(basename ${file%.gz})
+    # get quality report
     python quality_check.py \
-              --input_file ${RESULT_PATH}/$(basename ${file%.gz}) \
-              --output_dir ${RESULT_PATH}/$(basename ${file%.gz}) \
+              --input_file ${RESULT_PATH}/$(basename ${file}) \
+              --output_dir ${RESULT_PATH} \
+              --suffix $(basename ${file%.fasta.gz})
               --library_path ${BUSCO_DB} \
               --threads ${THREADS}
 done
