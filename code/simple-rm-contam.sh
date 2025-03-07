55a5924720a6755993366b5acee79847ff54e6ec
Yixuan Yang <yyang55@ncsu.edu>
2025-03-07 11:47:53 -0500

    Update simple-rm-contam.sh

--- code/simple-rm-contam.sh
+++ code/simple-rm-contam.sh
@@ -48,9 +48,9 @@ for file in ${FILES}; do
               --output ${RESULT_PATH}/$(basename ${file%.gz})
     # assemble the genome
     metaMDBG asm \
-        --out-dir ${RESULT_PATH}/${file%.gz}_asm \
-        --in-hifi ${RESULT_PATH}/$(basename ${file%.gz}) \
-        --threads ${THREADS}
+              --out-dir ${RESULT_PATH}/${file%.gz}_asm \
+              --in-hifi ${RESULT_PATH}/$(basename ${file%.gz}) \
+              --threads ${THREADS}
     mv ${RESULT_PATH}/${file%.gz}_asm/contigs.fasta.gz ${RESULT_PATH}/${file%.gz}
     gzip ${RESULT_PATH}/$(basename ${file%.gz})
     rm -rf ${RESULT_PATH}/${file%.gz}_asm
