3e30b764019e6d296320b7c7028a7d9d43eb3ff0
Yixuan Yang <yyang55@ncsu.edu>
2025-03-08 08:55:54 -0500

    Create fcs-kraken2-db.sh

---
+++ code/fcs-kraken2-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REF_SEQ=$HOME/project_data/downy/fcs-db/all.fasta
+KRAKEN_DB=$HOME/project_data/downy/KrakenDB/fcs-kraken2
+THREADS=24
+
+kraken2-build \
+    --build \
+    --db $KRAKEN_DB \
+    --add-to-library $REF_SEQ \
+    --threads $THREADS 
+    
+kraken2-inspect \
+    --db $KRAKEN_DB \
+    --use-mpa-style \
+    --threads $THREADS \
+    > $HOME/project_data/downy/KrakenDB/fcs-kraken2.txt
+    
+kraken2-build \
+    --db $KRAKEN_DB \
+    --clean 
