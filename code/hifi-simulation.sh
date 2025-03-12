79a22f890eb4b5f48778edc3efb2d483bf5ef76a
Yixuan Yang <yyang55@ncsu.edu>
2025-03-12 10:28:25 -0400

    Update hifi-simulation.sh

--- code/hifi-simulation.sh
+++ code/hifi-simulation.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-OUTPUT_FOLDER=$HOME/project_data/downy/hifi-simulation
-
 mess run \
     --input ../metagenome-simulation.tsv \
-    --output ${OUTPUT_FOLDER} \
+    --output $HOME/project_data/downy/hifi-simulation \
     --tech pacbio \
     --error hifi \
     --model QSHMM-ONT-HQ 
-    
-metaMDBG asm \
-    --out-dir ${OUTPUT_FOLDER} \
-    --in-hifi ${OUTPUT_FOLDER}/fastq/*.fq.gz \
-    --threads 24
