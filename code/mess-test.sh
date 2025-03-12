9558126171691efd585a96878a720476b94a0c9d
Yixuan Yang <yyang55@ncsu.edu>
2025-03-11 21:07:03 -0400

    Create mess-test.sh

---
+++ code/mess-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mess run \
+    --input ~/Projects/downy-mildew-assemble/metagenome-simulation.tsv \
+    --output ~/project_data/downy/simulated-hifi \
+    --threads 24 \
+    --nrank 20
