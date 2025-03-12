79cc239e99f243ebd2b0b3c2a982e1db7710a8b3
Yixuan Yang <yyang55@ncsu.edu>
2025-03-12 01:01:08 -0400

    Update mess-test.sh

--- code/mess-test.sh
+++ code/mess-test.sh
@@ -3,7 +3,6 @@
 mess run \
     --input ../metagenome-simulation.tsv \
     --output ~/project_data/downy/simulated-hifi \
-    --threads 24 \
     --tech pacbio \
     --error hifi \
     --model QSHMM-ONT-HQ \
