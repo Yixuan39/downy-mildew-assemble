87c718ba66a34e2e88f9fc6485b028d284a8a96a
Yixuan Yang <yyang55@ncsu.edu>
2025-03-16 12:47:25 -0400

    Update hifi-simulation.sh

--- code/hifi-simulation.sh
+++ code/hifi-simulation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mess run \
+    --threads 24 \
     --input ../metagenome-simulation.tsv \
     --output $HOME/project_data/downy/hifi-simulation \
     --tech pacbio \
     --error hifi \
-    --model QSHMM-ONT-HQ  
+    --model QSHMM-RSII  
