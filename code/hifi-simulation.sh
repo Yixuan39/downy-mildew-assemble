e495763c498e234e4fd2af50dad109425ee032dd
Yixuan Yang <yyang55@ncsu.edu>
2025-03-19 18:47:36 -0400

    Update hifi-simulation.sh

--- code/hifi-simulation.sh
+++ code/hifi-simulation.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-WD=/data/run/yyang
-
 mess run \
-    --threads 24 \
-    --input ../metagenome-simulation.tsv \
-    --output ${WD}/project_data/downy/hifi-simulation \
+    --input ../subsample.tsv \
+    --output $HOME/project_data/downy/hifi-simulation \
     --tech pacbio \
     --error hifi
