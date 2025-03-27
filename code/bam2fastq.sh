871ad4b595eec62c7d649e0d6d1c0e2c25f9d72c
Yixuan Yang <yyang55@ncsu.edu>
2025-03-27 14:06:10 -0400

    Create bam2fastq.sh

---
+++ code/bam2fastq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=bam2fq
+#SBATCH --array=0-2
+#SBATCH --cpus-per-task=24
+
+
+FILES=$(ls -R $HOME/project_data/downy/GSL_Data/5Feb24/**/*bam)
+FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
+OUTPUT_DIR=$HOME/project_data/downy/GSL_Data/fastq
+mkdir -p $OUTPUT_DIR
+
+bam2fastq --output $OUTPUT_DIR/$(dirname $FILE) $FILE
+
