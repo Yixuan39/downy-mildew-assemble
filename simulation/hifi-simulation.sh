806885780ae25eb42189bd7d7bd2bdac920f0552
Yixuan Yang <yyang55@ncsu.edu>
2025-04-29 14:24:27 -0400

    Update hifi-simulation.sh

--- simulation/hifi-simulation.sh
+++ simulation/hifi-simulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mess run \
-    --input ../simulation-info/subsample-prop.tsv \
+    --input ./information/subsample-prop.tsv \
     --output $HOME/project_data/downy/hifi-simulation \
     --threads 24 \
     --bases 10G \
