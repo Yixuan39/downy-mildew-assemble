20f3bb08abb94b2e0cd3d925d0193bcf3f3f3d3b
Yixuan Yang <yyang55@ncsu.edu>
2025-04-29 14:25:21 -0400

    Update hifi-simulation.sh

--- simulation/hifi-simulation.sh
+++ simulation/hifi-simulation.sh
@@ -3,7 +3,6 @@
 mess run \
     --input ./information/subsample-prop.tsv \
     --output $HOME/project_data/downy/hifi-simulation \
-    --threads 24 \
     --bases 10G \
     --tech pacbio \
     --model QSHMM-RSII \
