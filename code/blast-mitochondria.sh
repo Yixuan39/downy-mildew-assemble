a684a2db212ade3288c5e083881d14d6945f6917
Yixuan Yang (Jeff Thorne) <yyang55@sisko.statgen.ncsu.edu>
2025-09-20 23:31:03 -0400

    unplace and mitochondria contigs analysis

--- code/blast-mitochondria.sh
+++ code/blast-mitochondria.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 #SBATCH --array=0-2
-#SBATCH --cpus-per-task=32
 
 INPUT_DIR=$HOME/project_data/downy/hifiasm/fcs-gx/kraken2/purge_dups/rag_tag/unplaced-contigs
 RESULT_DIR=$HOME/project_data/downy/hifiasm/fcs-gx/kraken2/purge_dups/rag_tag/unplaced-contigs
 FILES=($(find "$INPUT_DIR" -type f -name "*.fasta"))
 FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
-THREADS=32
 mkdir -p ${RESULT_DIR}
 echo "Processing: $FILE"
 BASENAME=$(basename ${FILE})  
 BASENAME=${BASENAME%.fasta}
 
 blastn -query "$FILE" \
-  -db nt \
+  -subject "../data/KT072718.1.fna" \
   -outfmt "6 qseqid sseqid pident length qstart qend sstart send evalue staxids" \
   -max_target_seqs 1 \
   -max_hsps 1 \
-  -num_threads "$THREADS" \
-  -out "$RESULT_DIR/$BASENAME.nt.tsv"
-  
+  -out "$RESULT_DIR/$BASENAME.mito.tsv"
