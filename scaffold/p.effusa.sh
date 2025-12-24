99fd03993731ca388f42a0dc0cd4accc2f147e86
Yixuan Yang <71982982+Yixuan39@users.noreply.github.com>
2025-12-24 11:13:20 -0800

    Refactor scaffolds analysis and update BLAST filtering
    Updated analysis/scaffolds.Rmd to improve BLAST table parsing, add alignment length threshold (>= 3000), and enhance table arrangement and filtering. Changed the document title to 'Scaffolds' and updated the corresponding HTML output. Added a new shell script scaffold/p.effusa.sh.

---
+++ scaffold/p.effusa.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=rag_tag
+#SBATCH --cpus-per-task=24
+
+set -euo pipefail
+
+# with hifiasm results, run purge_dups on the primary assembly.
+INPUT=$HOME/project_data/downy/Assembly/p.effusa/SRR15142133.fasta.gz
+RESULT_DIR=$HOME/project_data/downy/Scaffold/p.effusa
+REF=$HOME/project_data/downy/oomycota-genome/Peronospora-effusa.fna
+BUSCO_DB=$HOME/project_data/downy/BUSCO_DB
+THREADS=24
+mkdir -p $RESULT_DIR
+
+
+gzip -dkc $INPUT > $INPUT.fasta
+
+ragtag.py scaffold \
+  -o $RESULT_DIR/ragtag \
+  -w \
+  -t $THREADS \
+  $REF \
+  $INPUT.fasta
+
+gzip -c $RESULT_DIR/ragtag/ragtag.scaffold.fasta > $RESULT_DIR/SRR15142133.fasta.gz
+rm -rf $RESULT_DIR/ragtag
+rm $INPUT.fasta
+
+python quality-check.py \
+  --input_file $RESULT_DIR/SRR15142133.fasta.gz \
+  --output_dir $RESULT_DIR \
+  --library_path $BUSCO_DB \
+  --threads $THREADS
