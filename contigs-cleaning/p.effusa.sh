e3d2f00e75e5757ee1b5fd065a32745fc395a917
Yixuan Yang <yyang55@ncsu.edu>
2026-01-31 21:29:04 -0500

    Update p.effusa.sh

--- contigs-cleaning/p.effusa.sh
+++ contigs-cleaning/p.effusa.sh
@@ -5,14 +5,14 @@
 set -euo pipefail
 
 # with hifiasm results, run purge_dups on the primary assembly.
-INPUT=$HOME/Projects/downy-mildew-assemble/data/cleaned_contigs/SRR15142133/*.fasta.gz
-RESULT_DIR=$HOME/Projects/downy-mildew-assemble/data/cleaned_contigs/SRR15142133
+INPUT=$HOME/Projects/downy-mildew-assemble/data/cleaned_contigs/p_effusa/*.fasta.gz
+RESULT_DIR=$HOME/Projects/downy-mildew-assemble/data/cleaned_contigs/p_effusa
 BUSCO_DB=$HOME/project_data/downy/BUSCO_DB
 THREADS=24
 mkdir -p $RESULT_DIR
 
 python quality-check.py \
-  --input_file $RESULT_DIR/SRR15142133.fasta.gz \
+  --input_file $RESULT_DIR/p_effusa.fasta.gz \
   --output_dir $RESULT_DIR \
   --library_path $BUSCO_DB \
   --threads $THREADS
