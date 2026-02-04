d59ab4dea01a7967ac9d0a6a0eadd5414996f947
Yixuan Yang <yyang55@ncsu.edu>
2026-02-04 16:22:27 -0500

    Simplify blast-all.sh; comment out nt megablast
    Remove explicit SBATCH --cpus-per-task and strip multithreading/extra HSP options from blastn invocation. Change -outfmt to numeric 6 (default tabular) and comment out the second, expensive megablast/nt search (previously produced *.nt.tsv). Keep gzip decompression and cleanup of temporary files. This reduces resource assumptions and disables the heavy nt database query while preserving the mitochondrial subject search.

--- code/blast-all.sh
+++ code/blast-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --array=0-3
-#SBATCH --cpus-per-task=32
 
 INPUT_DIR=$HOME/project_data/downy/Scaffold
 RESULT_DIR=$HOME/project_data/downy/BLAST
@@ -15,19 +14,17 @@ gzip -d -k ${FILE}
 
 blastn -query ${FILE%.gz} \
   -subject "../data/KT072718.1.fna" \
-  -outfmt "6 qseqid sseqid pident length qlen slen evalue staxids" \
+  -outfmt 6 \
   -max_target_seqs 1 \
-  -max_hsps 1 \
-  -num_threads "$THREADS" \
   -out "$RESULT_DIR/$BASENAME.mito.tsv"
 
-blastn -query ${FILE%.gz} \
-  -task megablast \
-  -db nt \
-  -outfmt "6 qseqid sseqid pident length qlen slen evalue staxids" \
-  -max_target_seqs 5 \
-  -max_hsps 1 \
-  -num_threads "$THREADS" \
-  -out "$RESULT_DIR/$BASENAME.nt.tsv"
+# blastn -query ${FILE%.gz} \
+#   -task megablast \
+#   -db nt \
+#   -outfmt "6 qseqid sseqid pident length qlen slen evalue staxids" \
+#   -max_target_seqs 5 \
+#   -max_hsps 1 \
+#   -num_threads "$THREADS" \
+#   -out "$RESULT_DIR/$BASENAME.nt.tsv"
   
 [ -f "${FILE%.gz}" ] && rm "${FILE%.gz}"
