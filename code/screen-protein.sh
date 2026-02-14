f533700210600899c42a01277bfc07a782cc3e7d
Yixuan Yang <yyang55@ncsu.edu>
2026-02-14 11:10:06 -0500

    Update screen-protein.sh

--- code/screen-protein.sh
+++ code/screen-protein.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --array=0-3
 #SBATCH -c 32
+#SBATCH --mem=0
 
 INPUT_DIR=$HOME/project_data/downy/Helixer
 RESULT_DIR=$HOME/project_data/downy/Helixer
@@ -16,10 +17,13 @@ start=$EPOCHREALTIME
 
 diamond blastp \
 --threads $THREADS \
---quiet \
+--evalue 0.00001 \
+--max-target-seqs 10 \
 --db $HOME/diamond_db/nr \
 --out $RESULT_DIR/$BASENAME.daa \
 --ultra-sensitive \
+--block-size 20 \
+--index-chunks 1 \
 --query $FILE \
 --outfmt 100
 
@@ -30,10 +34,7 @@ echo "Runtime: $runtime seconds"
 # convert result to tab format
 diamond view \
 --threads $THREADS \
---quiet \
 --header simple \
---evalue 0.00001 \
---max-target-seqs 10 \
 --out $RESULT_DIR/$BASENAME.tsv \
 --outfmt 6 qseqid sseqid pident length qlen slen evalue staxids qstart qend sstart send \
 --daa $RESULT_DIR/$BASENAME.daa \
@@ -42,9 +43,6 @@ diamond view \
 # convert result to pairwise alignment format
 diamond view \
 --threads $THREADS \
---quiet \
---evalue 0.00001 \
---max-target-seqs 10 \
 --out $RESULT_DIR/$BASENAME.txt \
 --outfmt 0 \
 --daa $RESULT_DIR/$BASENAME.daa \
