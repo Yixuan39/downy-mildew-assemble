581fbab5fe0735e1b153724c8b86adbe8f67b8ee
Yixuan Yang <yyang55@ncsu.edu>
2026-02-14 11:16:43 -0500

    Create screen-protein-fast.sh

---
+++ code/screen-protein-fast.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --array=0-3
+#SBATCH -c 24
+#SBATCH --mem=380G
+
+INPUT_DIR=$HOME/project_data/downy/Helixer
+RESULT_DIR=$HOME/project_data/downy/Helixer-fast
+FILES=($(find "$INPUT_DIR" -type f -name "*.faa"))
+FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
+THREADS=24
+mkdir -p ${RESULT_DIR}
+echo "Processing: $FILE"
+BASENAME=$(basename ${FILE})  
+BASENAME=${BASENAME%.faa}
+
+start=$EPOCHREALTIME
+
+diamond blastp \
+--threads $THREADS \
+--evalue 0.00001 \
+--max-target-seqs 10 \
+--db $HOME/diamond_db/nr \
+--out $RESULT_DIR/$BASENAME.daa \
+--fast \
+--block-size 20 \
+--index-chunks 1 \
+--query $FILE \
+--outfmt 100
+
+end=$EPOCHREALTIME
+runtime=$(echo "$end - $start" | bc)
+echo "Runtime: $runtime seconds"
+
+# convert result to tab format
+diamond view \
+--threads $THREADS \
+--header simple \
+--out $RESULT_DIR/$BASENAME.tsv \
+--outfmt 6 qseqid sseqid pident length qlen slen evalue staxids qstart qend sstart send \
+--daa $RESULT_DIR/$BASENAME.daa \
+--forwardonly
+
+# convert result to pairwise alignment format
+diamond view \
+--threads $THREADS \
+--out $RESULT_DIR/$BASENAME.txt \
+--outfmt 0 \
+--daa $RESULT_DIR/$BASENAME.daa \
+--forwardonly
