6a5c68a9968af5a2ff688d1ff80855712eebde1e
Yixuan Yang <yyang55@ncsu.edu>
2026-02-17 22:16:05 -0500

    Update genomeqc.sh

--- code/genomeqc.sh
+++ code/genomeqc.sh
@@ -10,6 +10,7 @@ nextflow run nf-core/genomeqc \
    -profile singularity \
    --input ../data/samplesheet.csv \
    --busco_lineage stramenopiles_odb12 \
+   --busco_lineages_path $HOME/busco_db/busco_downloads \
    --busco_clean \
    --gx_db $HOME/project_data/downy/fcs-db \
    --outdir $HOME/project_data/downy/genomeqc
