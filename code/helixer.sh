975ab10237094340801a6773178df138fa20b3b2
Yixuan Yang (Jeff Thorne) <yyang55@sisko.statgen.ncsu.edu>
2026-03-23 17:56:30 -0400

    remove '_' in seq name

--- code/helixer.sh
+++ code/helixer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --array=0-3
 #SBATCH -p gpu
-#SBATCH -c 16
+#SBATCH -c 8
 
 INPUT_DIR=$HOME/project_data/downy/Scaffold
 RESULT_DIR=$HOME/project_data/downy/Helixer
@@ -18,11 +18,15 @@ nvidia-smi
 apptainer run --nv ~/helixer-docker_helixer_v0.3.6_cuda_12.2.2-cudnn8.sif Helixer.py \
   --fasta-path ${FILE%.gz} --lineage fungi \
   --gff-output-path ${RESULT_DIR}/${BASENAME}.gff
+  
+sed -i 's/ID=_/ID=/g; s/Parent=_/Parent=/g' ${RESULT_DIR}/${BASENAME}.gff
 
 gffread \
   ${RESULT_DIR}/${BASENAME}.gff \
   -g ${FILE%.gz} \
   -y ${RESULT_DIR}/${BASENAME}.faa
+  
+
 
 rm ${FILE%.gz}
 rm ${FILE%.gz}.fai
