c783f5061cbe528b3ad15f1885acfc4d89c857a5
Yixuan Yang <yyang55@ncsu.edu>
2026-05-22 22:15:43 -0400

    remove useless script

--- analysis/run_orthofinder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -p bigmem
-#SBATCH -c 32
-#SBATCH --mem=0
-
-
-orthofinder -f "$HOME/project_data/downy/genespace/tmp" -t 32 -X -o "$HOME/project_data/downy/genespace/orthofinder"
