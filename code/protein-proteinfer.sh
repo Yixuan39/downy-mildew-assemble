6f63f16b0375c79919c54ec223f6efcea63fd283
Yixuan Yang (Jeff Thorne) <yyang55@sisko.statgen.ncsu.edu>
2026-05-24 18:42:49 -0400

    add proteInfer

---
+++ code/protein-proteinfer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --array=0-3
+#SBATCH -c 24
+#SBATCH --mem=0
+
+INPUT_DIR=$HOME/project_data/downy/Helixer-contigs
+RESULT_DIR=$HOME/project_data/downy/Helixer-contigs/proteinfer
+FILES=($(find "$INPUT_DIR" -type f -name "*.faa"))
+FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
+THREADS=24
+
+echo "Processing: $FILE"
+BASENAME=$(basename ${FILE})
+BASENAME=${BASENAME%.faa}
+mkdir -p ${RESULT_DIR}
+
+start=$EPOCHREALTIME
+
+python3 ${HOME}/software/proteinfer/proteinfer.py \
+--i ${FILE} \
+--o ${RESULT_DIR}/${BASENAME}.proteinfer_thr09.tsv \
+--model_cache_path ${HOME}/software/proteinfer/cached_models \
+--num_ensemble_elements 5 \
+--reporting_threshold 0.9
+
+end=$EPOCHREALTIME
+runtime=$(echo "$end - $start" | bc)
