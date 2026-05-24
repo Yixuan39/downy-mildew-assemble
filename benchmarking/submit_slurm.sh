37bcc74600d741b617b4a0ef2283f7fff171c5b5
Yixuan Yang <yyang55@ncsu.edu>
2026-05-24 15:24:06 -0400

    Update submit_slurm.sh

--- benchmarking/submit_slurm.sh
+++ benchmarking/submit_slurm.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 sbatch --export=ALL,SAMPLE=p_effusa hifiasm.sh
 sbatch --export=ALL,SAMPLE=p_effusa hifiasm_blastn.sh
-sbatch -p bigmem -w node 95 --export=ALL,SAMPLE=p_effusa TEA.sh
+sbatch -p bigmem -w node95 --export=ALL,SAMPLE=p_effusa TEA.sh
 
 sbatch --export=ALL,SAMPLE=MSU1 hifiasm.sh
 sbatch --export=ALL,SAMPLE=MSU1 hifiasm_blastn.sh
-sbatch -p bigmem -w node 95 --export=ALL,SAMPLE=MSU1,METHOD=tea_no_downsample TEA.sh
-sbatch -p bigmem -w node 95 --export=ALL,SAMPLE=MSU1,METHOD=tea_downsample TEA.sh
+sbatch -p bigmem -w node95 --export=ALL,SAMPLE=MSU1,METHOD=tea_no_downsample TEA.sh
+sbatch -p bigmem -w node95 --export=ALL,SAMPLE=MSU1,METHOD=tea_downsample TEA.sh
