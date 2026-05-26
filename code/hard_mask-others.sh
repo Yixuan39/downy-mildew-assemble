e213ce1ac74fd1bff2462dce9481f71a9ee820c2
Yixuan Yang <yyang55@ncsu.edu>
2026-05-26 15:08:00 -0400

    fallback to repeatmasker

--- code/hard_mask-others.sh
+++ code/hard_mask-others.sh
@@ -11,25 +11,28 @@ FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
 echo "Processing: $FILE"
 BASENAME=$(basename ${FILE})  
 BASENAME=${BASENAME%.fna.gz}
-EG_NAME=${BASENAME##*_}
-TMP_DIR=${RESULT_DIR}/${EG_NAME}_tmp
+TMP_DIR=${RESULT_DIR}/${BASENAME}_tmp
 # decompress the original file to result directory
 mkdir -p ${TMP_DIR}
-gzip -dc "$FILE" > "${TMP_DIR}/${EG_NAME}.fna"
+gzip -dc "$FILE" > "${TMP_DIR}/${BASENAME}.fasta"
 
 
-mamba run -n earlGrey earlGrey \
-  -g "${TMP_DIR}/${EG_NAME}.fna" \
-  -s $EG_NAME \
-  -o $RESULT_DIR \
-  -t $THREADS \
-  -r eukaryota \
-  -d yes \
-  -q yes
+BuildDatabase \
+  -name ${TMP_DIR}/db \
+  ${TMP_DIR}/${BASENAME}.fna
 
-mamba run -n earlGrey bedtools maskfasta \
-  -fi "${TMP_DIR}/${EG_NAME}.fna" \
-  -bed $RESULT_DIR/${EG_NAME}_EarlGrey/${EG_NAME}_summaryFiles/${EG_NAME}.filteredRepeats.bed \
-  -fo $RESULT_DIR/${BASENAME}.fna
+RepeatModeler \
+  -threads 32 \
+  -database ${TMP_DIR}/db > ${TMP_DIR}/${BASENAME}.out
 
-mamba run -n earlGrey gzip $RESULT_DIR/${BASENAME}.fna
+RepeatMasker \
+  -engine ncbi \
+  -parallel 8 \
+  -gff \
+  -lib ${TMP_DIR}/db-families.fa \
+  -dir ${RESULT_DIR}/${BASENAME}_masked \
+  ${TMP_DIR}/${BASENAME}.fna
+
+cp ${RESULT_DIR}/${BASENAME}_masked/${BASENAME}.fna.masked ${RESULT_DIR}/${BASENAME}.fna
+gzip ${RESULT_DIR}/${BASENAME}.fna
+
