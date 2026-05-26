81621b6b99114b1df8771688e4b59f6e87f281ed
Yixuan Yang <yyang55@ncsu.edu>
2026-05-25 22:15:42 -0400

    Update hard_mask-others.sh

--- code/hard_mask-others.sh
+++ code/hard_mask-others.sh
@@ -6,20 +6,20 @@
 THREADS=32
 INPUT_DIR=$HOME/project_data/downy/downy-mildew-genomes
 RESULT_DIR=$HOME/project_data/downy/downy-mildew-genomes/hardmasked
-FILES=($(find "$INPUT_DIR" -type f -name "*.fasta.gz"))
+FILES=($(find "$INPUT_DIR" -type f -name "*.fna.gz"))
 FILE=${FILES[$SLURM_ARRAY_TASK_ID]}
 echo "Processing: $FILE"
 BASENAME=$(basename ${FILE})  
-BASENAME=${BASENAME%.fasta.gz}
+BASENAME=${BASENAME%.fna.gz}
 EG_NAME=${BASENAME##*_}
 TMP_DIR=${RESULT_DIR}/${EG_NAME}_tmp
 # decompress the original file to result directory
 mkdir -p ${TMP_DIR}
-gzip -dc "$FILE" > "${TMP_DIR}/${EG_NAME}.fasta"
+gzip -dc "$FILE" > "${TMP_DIR}/${EG_NAME}.fna"
 
 
 mamba run -n earlGrey earlGrey \
-  -g "${TMP_DIR}/${EG_NAME}.fasta" \
+  -g "${TMP_DIR}/${EG_NAME}.fna" \
   -s $EG_NAME \
   -o $RESULT_DIR \
   -t $THREADS \
@@ -28,8 +28,8 @@ mamba run -n earlGrey earlGrey \
   -q yes
 
 mamba run -n earlGrey bedtools maskfasta \
-  -fi "${TMP_DIR}/${EG_NAME}.fasta" \
+  -fi "${TMP_DIR}/${EG_NAME}.fna" \
   -bed $RESULT_DIR/${EG_NAME}_EarlGrey/${EG_NAME}_summaryFiles/${EG_NAME}.filteredRepeats.bed \
-  -fo $RESULT_DIR/${BASENAME}.fasta
+  -fo $RESULT_DIR/${BASENAME}.fna
 
-mamba run -n earlGrey gzip $RESULT_DIR/${BASENAME}.fasta
+mamba run -n earlGrey gzip $RESULT_DIR/${BASENAME}.fna
