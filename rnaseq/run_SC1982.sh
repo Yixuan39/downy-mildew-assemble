7c95c0023119ac545176ea98b675deeee3ce152f
Yixuan Yang <yyang55@ncsu.edu>
2026-06-06 14:55:44 -0400

    Use Parent for featurecounts and enable biotype QC
    Update three rnaseq run scripts to change --featurecounts_group_type from gene_id to Parent and remove the --skip_biotype_qc flag so biotype QC runs. Files modified: rnaseq/run_MSU1.sh, rnaseq/run_OR502AA.sh, rnaseq/run_SC1982.sh. This ensures featureCounts groups by Parent and restores biotype quality checks.

--- rnaseq/run_SC1982.sh
+++ rnaseq/run_SC1982.sh
@@ -6,9 +6,8 @@ nextflow run nf-core/rnaseq -r 3.26.0 \
     --gff $HOME/project_data/downy/contigs-renamed/helixer/Pseudoperonospora_cubensis_SC1982.gff \
     --fasta $HOME/project_data/downy/contigs-renamed/cleaned/Pseudoperonospora_cubensis_SC1982.fasta.gz \
     --contaminant_screening 'kraken2_bracken' --kraken_db $HOME/db/kraken2/PlusPFP \
-    --featurecounts_group_type gene_id \
+    --featurecounts_group_type Parent \
     --save_align_intermeds \
-    --skip_biotype_qc \
     --min_mapped_reads 0 \
     -profile apptainer \
     -c custom.config
