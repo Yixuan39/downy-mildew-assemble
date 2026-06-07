94da49a1f6f5756e19739236e690c978d3db3604
Yixuan Yang <yyang55@ncsu.edu>
2026-06-07 10:41:45 -0400

    Update run_MSU1.sh

--- rnaseq/run_MSU1.sh
+++ rnaseq/run_MSU1.sh
@@ -6,8 +6,10 @@ nextflow run nf-core/rnaseq -r 3.26.0 \
     --gff $HOME/project_data/downy/contigs-renamed/helixer/Pseudoperonospora_cubensis_MSU1.gff \
     --fasta $HOME/project_data/downy/contigs-renamed/cleaned/Pseudoperonospora_cubensis_MSU1.fasta.gz \
     --contaminant_screening 'kraken2_bracken' --kraken_db $HOME/db/kraken2/PlusPFP \
-    --featurecounts_group_type Parent \
+    --skip_biotype_qc false \
+    --featurecounts_feature_type exon \
+    --featurecounts_group_type transcript_id \
     --save_align_intermeds \
     --min_mapped_reads 0 \
     -profile apptainer \
-    -c custom.config
+    -c custom.config -resume
