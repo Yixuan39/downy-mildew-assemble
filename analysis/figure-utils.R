1aff499ea6deca045832cd62a34d3ec36cc5aabb
Yixuan Yang <yyang55@ncsu.edu>
2026-06-20 08:36:05 -0400

    Create figure-utils.R

---
+++ analysis/figure-utils.R
@@ -0,0 +1,70 @@
+figure_path <- function(filename) {
+  if (requireNamespace("here", quietly = TRUE)) {
+    path <- here::here("figures", filename)
+  } else {
+    path <- file.path("figures", filename)
+  }
+  dir.create(dirname(path), recursive = TRUE, showWarnings = FALSE)
+  path
+}
+
+save_pub_r <- function(
+  plot,
+  filename,
+  width_mm,
+  height_mm,
+  dpi = 600,
+  allowed_width_mm = c(89, 183),
+  max_height_mm = 247,
+  allow_nonstandard = FALSE
+) {
+  if (!allow_nonstandard && !any(abs(width_mm - allowed_width_mm) < 0.01)) {
+    stop(
+      "Nature export width must be 89 mm or 183 mm. ",
+      "Got ", width_mm, " mm for ", filename, ".",
+      call. = FALSE
+    )
+  }
+  if (height_mm > max_height_mm) {
+    stop(
+      "Figure height exceeds ", max_height_mm, " mm: ",
+      height_mm, " mm for ", filename, ".",
+      call. = FALSE
+    )
+  }
+
+  base <- tools::file_path_sans_ext(figure_path(filename))
+  width_in <- width_mm / 25.4
+  height_in <- height_mm / 25.4
+
+  ggplot2::ggsave(
+    filename = paste0(base, ".svg"),
+    plot = plot,
+    device = svglite::svglite,
+    width = width_in,
+    height = height_in,
+    units = "in"
+  )
+
+  ggplot2::ggsave(
+    filename = paste0(base, ".pdf"),
+    plot = plot,
+    device = grDevices::pdf,
+    width = width_in,
+    height = height_in,
+    units = "in",
+    family = "Helvetica",
+    useDingbats = FALSE
+  )
+
+  ggplot2::ggsave(
+    filename = paste0(base, ".tiff"),
+    plot = plot,
+    device = ragg::agg_tiff,
+    width = width_in,
+    height = height_in,
+    units = "in",
+    dpi = dpi,
+    compression = "lzw"
+  )
+}
